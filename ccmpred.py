d5e6ccf1d75e2e76f6d785726d4fa614651a1d12
Stefan Seemayer <stefan@seemayer.de>
2015-03-16 10:19:20 +0100

    Start on command line interface

--- ccmpred.py
+++ ccmpred.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import optparse
 
 import ccmpred.weighting as cw
 import ccmpred.io.alignment as aln
@@ -9,13 +10,40 @@ import ccmpred.objfun.cd as cd
 import ccmpred.algorithm.gradient_descent as gd
 import ccmpred.algorithm.conjugate_gradients as cg
 
-msa = aln.read_msa_psicov("/home/seemayer/Projects/CCMpredNew/data/1atzA.aln")
-weights = cw.weights_simple(msa)
+OBJECTIVE_FUNCTIONS = {
+    "pll": pll.PseudoLikelihood,
+    "pcd": cd.ContrastiveDivergence,
+}
 
-x0, of = pll.PseudoLikelihood.init_from_default(msa, weights)
-#x0, of = cd.ContrastiveDivergence.init_from_default(msa, weights)
+ALGORITHMS = {
+    "gradient_descent": lambda of, x0, opt: gd.minimize(of, x0, opt.numiter, step=lambda i: 1e-3 / (1 + i / 25)),
+    "conjugate_gradients": lambda of, x0, opt: cg.minimize(of, x0, opt.numiter),
+}
 
-# fx, x = cg.minimize(of, x0, 100)
-fx, x = gd.minimize(of, x0, 100, step=lambda i: 1e-3 / (1 + i / 25))
 
-print("Finished with fx = {fx}".format(fx=fx))
+def main():
+    parser = optparse.OptionParser(usage="%prog [options] alnfile matfile")
+    parser.add_option("--objective-function", dest="objfun", default="pcd", choices=list(OBJECTIVE_FUNCTIONS.keys()), help="Specify the objective function ({0}) to optimize [default: \"%default\"]".format(", ".join(OBJECTIVE_FUNCTIONS.keys())))
+    parser.add_option("--algorithm", dest="algorithm", default="gradient_descent", choices=list(ALGORITHMS.keys()), help="Specify the algorithm ({0}) for optimization [default: \"%default\"]".format(", ".join(ALGORITHMS.keys())))
+    parser.add_option("-n", "--num-iterations", dest="numiter", default=100, type=int, help="Specify the number of iterations [default: %default]")
+
+    opt, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error("Need exactly 2 positional arguments!")
+
+    alnfile, matfile = args
+
+    print(opt)
+
+    msa = aln.read_msa_psicov(alnfile)
+    weights = cw.weights_simple(msa)
+
+    x0, of = OBJECTIVE_FUNCTIONS[opt.objfun].init_from_default(msa, weights)
+
+    fx, x = ALGORITHMS[opt.algorithm](of, x0, opt)
+
+    print("Finished with fx = {fx}".format(fx=fx))
+
+
+if __name__ == '__main__':
+    main()
