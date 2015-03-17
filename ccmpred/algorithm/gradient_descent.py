f33020f0063201981db437e582b9937752a6c798
Stefan Seemayer <stefan@seemayer.de>
2015-03-17 09:07:12 +0100

    Report fx and gradient at 0th iteration of gradient descent

--- ccmpred/algorithm/gradient_descent.py
+++ ccmpred/algorithm/gradient_descent.py
@@ -5,16 +5,19 @@ def minimize(objfun, x, maxiter, alpha0=None, alpha_decay=10):
 
     objfun.begin_progress()
 
+    fx, g = objfun.evaluate(x)
+    objfun.progress(x, g, fx, 0, 1, 0)
+
+    if not alpha0:
+        alpha0 = 1 / np.sqrt(np.sum(g * g))
+
     for i in range(maxiter):
+        alpha = alpha0 / (1 + i / alpha_decay)
         fx, g = objfun.evaluate(x)
 
-        if not alpha0:
-            alpha0 = 1 / np.sqrt(np.sum(g * g))
-
-        alpha = alpha0 / (1 + i / alpha_decay)
-
-        objfun.progress(x, g, fx, i, 1, alpha)
+        objfun.progress(x, g, fx, i + 1, 1, alpha)
 
         x -= alpha * g
+
 
     return fx, x
