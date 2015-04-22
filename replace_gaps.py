842a95224713ff967435bd43e4b813b4005fae49
Stefan Seemayer <stefan@seemayer.de>
2015-04-22 17:54:56 +0200

    Removed --with-gaps option since columns are dependent on another

--- replace_gaps.py
+++ replace_gaps.py
@@ -1,26 +1,13 @@
 #!/usr/bin/env python
 import optparse
-import numpy as np
 
 import ccmpred.io.alignment
-import ccmpred.raw
 import ccmpred.gaps
-
-
-def cb_mrf(option, opt, value, parser):
-    raw = ccmpred.raw.parse(value)
-
-    def replace_mrf(msa):
-        # TODO compute probs
-        return msa
-
-    parser.values.replacement = replace_mrf
 
 
 def main():
     parser = optparse.OptionParser(usage="%prog [options] msa_in_file msa_out_file")
 
-    parser.add_option("--with-mrf", dest="replacement", action="callback", callback=cb_mrf, type=str, nargs=1, metavar="MRFFILE", help="Remove gaps with character probabilities from MRF model")
     parser.add_option("--with-consensus", dest="replacement", action="store_const", const=ccmpred.gaps.remove_gaps_consensus, help="Remove gaps with consensus characters")
     parser.add_option("--with-col-freqs", dest="replacement", action="store_const", const=ccmpred.gaps.remove_gaps_col_freqs, help="Remove gaps with column character frequencies")
     parser.add_option("--msa-in-format", dest="msa_in_format", default="psicov", help="Input alignment format [default: '%default']")
