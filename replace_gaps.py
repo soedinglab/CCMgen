3994b7929daa01d989f2de18b0e5675e194ca72b
Stefan Seemayer <stefan@seemayer.de>
2015-04-22 17:39:32 +0200

    Simplify replace_gaps.py

--- replace_gaps.py
+++ replace_gaps.py
@@ -8,39 +8,31 @@ import ccmpred.counts
 import ccmpred.gaps
 
 
-def replacement_mrf(option, opt, value, parser):
+def cb_mrf(option, opt, value, parser):
     raw = ccmpred.raw.parse(value)
 
-    def inner(msa):
+    def replace_mrf(msa):
         # TODO compute probs
         return msa
 
-    parser.values.replacement = inner
+    parser.values.replacement = replace_mrf
 
 
-def replacement_consensus(option, opt, value, parser):
-    parser.values.replacement = ccmpred.gaps.remove_gaps_consensus
+def replace_col_freqs(msa):
+    counts = ccmpred.counts.single_counts(msa)
+    counts[:, 20] = 0
 
+    counts /= np.sum(counts, axis=1)[:, np.newaxis]
 
-def replacement_col_freqs(option, opt, value, parser):
-
-    def inner(msa):
-        counts = ccmpred.counts.single_counts(msa)
-        counts[:, 20] = 0
-
-        counts /= np.sum(counts, axis=1)[:, np.newaxis]
-
-        return ccmpred.gaps.remove_gaps_probs(msa, counts)
-
-    parser.values.replacement = inner
+    return ccmpred.gaps.remove_gaps_probs(msa, counts)
 
 
 def main():
     parser = optparse.OptionParser(usage="%prog [options] msa_in_file msa_out_file")
 
-    parser.add_option("--with-mrf", dest="replacement", action="callback", callback=replacement_mrf, type=str, nargs=1, metavar="MRFFILE", help="Remove gaps with character probabilities from MRF model")
-    parser.add_option("--with-consensus", dest="replacement", action="callback", callback=replacement_consensus, help="Remove gaps with consensus characters")
-    parser.add_option("--with-col-freqs", dest="replacement", action="callback", callback=replacement_col_freqs, help="Remove gaps with column character frequencies")
+    parser.add_option("--with-mrf", dest="replacement", action="callback", callback=cb_mrf, type=str, nargs=1, metavar="MRFFILE", help="Remove gaps with character probabilities from MRF model")
+    parser.add_option("--with-consensus", dest="replacement", action="store_const", const=ccmpred.gaps.remove_gaps_consensus, help="Remove gaps with consensus characters")
+    parser.add_option("--with-col-freqs", dest="replacement", action="store_const", const=replace_col_freqs, help="Remove gaps with column character frequencies")
     parser.add_option("--msa-in-format", dest="msa_in_format", default="psicov", help="Input alignment format [default: '%default']")
 
     opt, args = parser.parse_args()
