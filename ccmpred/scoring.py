9a91fff20241496070f78c5bbdac181f277e7817
Stefan Seemayer <stefan@seemayer.de>
2015-06-22 10:39:32 +0200

    Add APC function, ignore gap potentials in frobenius norm

--- ccmpred/scoring.py
+++ ccmpred/scoring.py
@@ -5,5 +5,12 @@ def frobenius_score(x):
     means = np.mean(np.mean(x, axis=2), axis=2)
 
     x_centered = x - means[:, :, np.newaxis, np.newaxis]
+    x_centered = x_centered[:, :, :20, :20]
 
     return np.sqrt(np.sum(x_centered * x_centered, axis=(2, 3)))
+
+
+def apc(cmat):
+    mean = np.mean(cmat, axis=0)
+    apc_term = mean[:, np.newaxis] * mean[np.newaxis, :] / np.mean(cmat)
+    return cmat - apc_term
