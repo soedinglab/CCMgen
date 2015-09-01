3e1056db6bb59ebcc6e67d386bd46c07b10d2e6f
Stefan Seemayer <stefan@seemayer.de>
2015-09-01 16:57:55 +0200

    Add sampling neff values from mutation rates, setting mutation rate for target neff

--- generate_sequences.py
+++ generate_sequences.py
@@ -5,7 +5,9 @@ import ccmpred.objfun.cd as cd
 import ccmpred.objfun.treecd as treecd
 import ccmpred.raw
 import ccmpred.trees
+import ccmpred.weighting
 import Bio.Phylo
+import sys
 
 
 def cb_tree_newick(option, opt_str, value, parser, *args, **kwargs):
@@ -46,11 +48,23 @@ def cb_seq0_mrf(option, opt_str, value, parser, *args, **kwargs):
     parser.values.seq0_source = get_seq0_mrf
 
 
+def cb_mutation_rate_neff_aln(option, opt_str, value, parser, *args, **kwargs):
+    msa = ccmpred.io.alignment.read_msa(value, parser.values.aln_format)
+    neff = np.sum(ccmpred.weighting.weights_simple(msa))
+
+    print("Target Neff is {0}".format(neff))
+
+    parser.values.mutation_rate_neff = neff
+
+
 def get_options():
     import optparse
     parser = optparse.OptionParser(usage="%prog [options] rawfile outalignment")
     parser.add_option("--aln-format", dest="aln_format", default="fasta", help="Specify format for alignment files [default: \"%default\"]")
     parser.add_option("--mutation-rate", dest="mutation_rate", default=20, type=float, help="Specify mutation rate [default: %default]")
+    parser.add_option("--mutation-rate-sample", dest="mutation_rate_sample", default=False, action="store_true", help="Sample mutation rates and Neffs")
+    parser.add_option("--mutation-rate-neff", dest="mutation_rate_neff", type=float, metavar="NEFF", help="Set mutation rate to generate alignment of given number of effective sequences")
+    parser.add_option("--mutation-rate-neff-aln", action="callback", callback=cb_mutation_rate_neff_aln, nargs=1, type=str, metavar="ALNFILE", help="Set mutation rate to generate alignment with same number of effective sequences as given alignment")
 
     parser.add_option("--tree-newick", dest="tree_source", default=None, metavar="DNDFILE", action="callback", nargs=1, type=str, callback=cb_tree_newick, help="Load tree from newick-formatted file DNDFILE")
     parser.add_option("--tree-binary", metavar="SPLITS DEPTH", action="callback", nargs=2, type=str, callback=cb_tree_binary, help="Generate binary tree with 2^SPLITS sequences and total depth DEPTH")
@@ -110,18 +124,61 @@ def main():
 
     depth_min, depth_max = get_child_depth_range(tree.clade)
 
-    print("Got tree with depth_min={0}, depth_max={1}, mutation_rate={2}".format(depth_min, depth_max, opt.mutation_rate))
-
     n_children = np.array([len(c.clades) for c in tree_bfs], dtype='uint64')
     branch_lengths = np.array([c.branch_length for c in tree_bfs], dtype=np.dtype('float64'))
 
     n_vertices = len(tree_bfs)
     n_leaves = len(tree.get_terminals())
 
+    print("Got tree with {3} leaves, depth_min={0}, depth_max={1}, mutation_rate={2}".format(depth_min, depth_max, opt.mutation_rate, n_leaves))
+
     x = cd.structured_to_linear(raw.x_single, raw.x_pair)
 
-    msa_sampled = np.empty((n_leaves, raw.ncol), dtype="uint8")
-    msa_sampled = treecd.cext.mutate_along_tree(msa_sampled, n_children, branch_lengths, x, n_vertices, seq0, opt.mutation_rate)
+    if opt.mutation_rate_sample:
+
+        msa_sampled = np.empty((n_leaves, raw.ncol), dtype="uint8")
+
+        def sample_neff(mutation_rate):
+
+            treecd.cext.mutate_along_tree(msa_sampled, n_children, branch_lengths, x, n_vertices, seq0, mutation_rate)
+            neff = np.sum(ccmpred.weighting.weights_simple(msa_sampled))
+
+            print(mutation_rate, neff)
+            sys.stdout.flush()
+
+        print("x y")
+        for _ in range(3):
+            for mr in np.arange(0, 4.81, 0.4):
+                sample_neff(mr)
+
+    else:
+
+        if opt.mutation_rate_neff:
+            # Neff is dependant on the evolutionary distance D by the following
+            # relationship:
+
+            # Neff = 1 + (N - 1) / (1 + exp(-a*(D-t)^3 + b*(D-t)))
+
+            # where N is the number of sampled leaves and a, b and t are
+            # parameters fitted from a regression.
+            a = -1.238
+            b = 1.033
+            t = 1.547
+
+            # we substitute z = D - t and obtain the polynomial:
+            # a*z^3 + b*z + log((N - 1) / (Neff - 1) - 1) = 0
+            # solve using numpy.roots
+            neffratio = np.log((n_leaves - 1) / (opt.mutation_rate_neff - 1) - 1)
+            roots = np.roots([a, 0, b, neffratio])
+
+            # only keep real solutions and resubstitute D = z + t
+            mutation_rate = np.real(roots[np.abs(np.imag(roots)) < 1e-5])[0] + t
+
+        else:
+            mutation_rate = opt.mutation_rate
+
+        msa_sampled = np.empty((n_leaves, raw.ncol), dtype="uint8")
+        msa_sampled = treecd.cext.mutate_along_tree(msa_sampled, n_children, branch_lengths, x, n_vertices, seq0, mutation_rate)
 
     with open(outalnfile, "w") as f:
         ccmpred.io.alignment.write_msa_psicov(f, msa_sampled)
