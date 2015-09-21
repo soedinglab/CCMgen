79fce3be369ca8328eca5ee55c37626f6e31cf7d
Stefan Seemayer <stefan@seemayer.de>
2015-09-21 15:46:14 +0200

    Better point sampling strategy for neff fitting

--- ccmpred/sampling/neff.py
+++ ccmpred/sampling/neff.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 import scipy.optimize
 import functools
+import operator
 
 import ccmpred.weighting
 import ccmpred.objfun.treecd as treecd
@@ -30,28 +31,54 @@ def model(x, a, b, t, n_leaves):
     return 1 + (n_leaves - 1) / (1 + np.exp(-a * z ** 3 + b * z))
 
 
-def fit_neff_model(branch_lengths, n_children, n_vertices, n_leaves, ncol, x, seq0, n_reps=1, mr_samples=np.arange(0, 0.6, 0.01), start_parameters=RFIT_PARAMETERS):
+def fit_neff_model(branch_lengths, n_children, n_vertices, n_leaves, ncol, x, seq0, start_parameters=RFIT_PARAMETERS):
     """Fit model parameters by computing Neff for some sample points"""
     ns = neff_sampler(branch_lengths, n_children, n_vertices, n_leaves, ncol, x, seq0)
 
-    mrs = np.array([])
-    for r in range(n_reps):
-        mrs = np.concatenate((mrs, np.array(mr_samples)))
+    mrs = [0]
+    nfs = [1]
 
-    nfs = np.array([ns(mr) for mr in mrs])
+    def find_min_mr_for_max_neff(threshold=0.999):
+        # sampling strategy 1: grow mutation rate until we get independent sequences
+        mrmax = 0.1
+        neffmax = ns(mrmax)
+        mrs.append(mrmax)
+        nfs.append(neffmax)
+        while neffmax < n_leaves * threshold:
+            mrmax *= 2
+            neffmax = ns(mrmax)
+            mrs.append(mrmax)
+            nfs.append(neffmax)
+
+    def subdivide(n_divisions=10):
+        # sampling strategy 2: sample midpoints for intervals with highest delta-y
+        for _ in range(n_divisions):
+
+            # find section with highest delta-y
+            deltas = [b - a for a, b in zip(nfs, nfs[1:])]
+            splitpos = max(((delta, i) for i, delta in enumerate(deltas)), key=operator.itemgetter(0))[1]
+
+            # calculate midpoint and sample corresponding Neff
+            xm = (mrs[splitpos] + mrs[splitpos + 1]) / 2
+            nm = ns(xm)
+
+            mrs.insert(splitpos + 1, xm)
+            nfs.insert(splitpos + 1, nm)
+
+    find_min_mr_for_max_neff()
+    subdivide()
+
+    # for mr, nf in zip(mrs, nfs):
+    #     print("{0}\t{1}".format(mr, nf))
 
     f = functools.partial(model, n_leaves=n_leaves)
 
     popt, pcov = scipy.optimize.curve_fit(f, mrs, nfs, p0=(RFIT_PARAMETERS['a'], RFIT_PARAMETERS['b'], RFIT_PARAMETERS['t']))
 
-    for mr, nf in zip(mrs, nfs):
-        print("{0}\t{1}\t{2}".format(mr, nf, f(mr, *popt)))
+    # for mr, nf in zip(mrs, nfs):
+    #     print("{0}\t{1}\t{2}".format(mr, nf, f(mr, *popt)))
 
-    mdl = {
-        'a': popt[0],
-        'b': popt[1],
-        't': popt[2]
-    }
+    mdl = dict(zip('abt', popt))
 
     print("Fit model a={a}, b={b}, t={t}".format(**mdl))
 
