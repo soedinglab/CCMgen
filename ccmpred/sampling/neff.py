a8d246c5052aaf6bf29c5ef02d86fca2d225024d
Stefan Seemayer <stefan@seemayer.de>
2015-09-22 09:29:06 +0200

    Simplified fitting model

--- ccmpred/sampling/neff.py
+++ ccmpred/sampling/neff.py
@@ -11,9 +11,9 @@ import ccmpred.objfun.treecd as treecd
 Neff is dependant on the evolutionary distance D by the following
 relationship:
 
-Neff = 1 + (N - 1) / (1 + exp(-a*(D-t)^3 + b*(D-t)))
+Neff = 1 + (N - 1) / (1 + exp((t - D) / a))
 
-where N is the number of sampled leaves and a, b and t are
+where N is the number of sampled leaves and a and t are
 parameters fitted from a regression.
 """
 
@@ -21,17 +21,15 @@ parameters fitted from a regression.
 # starting parameters from a global fit of a few protein families
 RFIT_PARAMETERS = {
     'a': 0.7513,
-    'b': -1.1308,
     't': 1.6381
 }
 
 
-def model(x, a, b, t, n_leaves):
-    z = x - t
-    return 1 + (n_leaves - 1) / (1 + np.exp(-a * z ** 3 + b * z))
+def model(x, a, t, n_leaves):
+    return 1 + (n_leaves - 1) / (1 + np.exp((t - x) / a))
 
 
-def fit_neff_model(branch_lengths, n_children, n_vertices, n_leaves, ncol, x, seq0, start_parameters=RFIT_PARAMETERS):
+def fit_neff_model(branch_lengths, n_children, n_vertices, n_leaves, ncol, x, seq0, n_reps=10, start_parameters=RFIT_PARAMETERS):
     """Fit model parameters by computing Neff for some sample points"""
     ns = neff_sampler(branch_lengths, n_children, n_vertices, n_leaves, ncol, x, seq0)
 
@@ -50,37 +48,49 @@ def fit_neff_model(branch_lengths, n_children, n_vertices, n_leaves, ncol, x, se
             mrs.append(mrmax)
             nfs.append(neffmax)
 
-    def subdivide(n_divisions=10):
-        # sampling strategy 2: sample midpoints for intervals with highest delta-y
+    def subdivide(n_divisions=20):
+
+        # sampling strategy 2: sample midpoints for intervals with highest delta-x
         for _ in range(n_divisions):
 
-            # find section with highest delta-y
-            deltas = [b - a for a, b in zip(nfs, nfs[1:])]
+            # find section with highest delta-x
+            deltas = [b - a for a, b in zip(mrs, mrs[1:])]
             splitpos = max(((delta, i) for i, delta in enumerate(deltas)), key=operator.itemgetter(0))[1]
 
             # calculate midpoint and sample corresponding Neff
             xm = (mrs[splitpos] + mrs[splitpos + 1]) / 2
-            nm = ns(xm)
+            ym = ns(xm)
 
             mrs.insert(splitpos + 1, xm)
-            nfs.insert(splitpos + 1, nm)
+            nfs.insert(splitpos + 1, ym)
 
     find_min_mr_for_max_neff()
     subdivide()
 
-    # for mr, nf in zip(mrs, nfs):
-    #     print("{0}\t{1}".format(mr, nf))
-
     f = functools.partial(model, n_leaves=n_leaves)
 
-    popt, pcov = scipy.optimize.curve_fit(f, mrs, nfs, p0=(RFIT_PARAMETERS['a'], RFIT_PARAMETERS['b'], RFIT_PARAMETERS['t']))
+    try:
+        popt, pcov = scipy.optimize.curve_fit(f, mrs, nfs, p0=(1.0, 1.0))
+        #popt, pcov = scipy.optimize.curve_fit(f, mrs, nfs, p0=(RFIT_PARAMETERS['a'], RFIT_PARAMETERS['b'], RFIT_PARAMETERS['t']))
+        #popt = [RFIT_PARAMETERS[p] for p in 'abt']
 
-    # for mr, nf in zip(mrs, nfs):
-    #     print("{0}\t{1}\t{2}".format(mr, nf, f(mr, *popt)))
+        with open("test.tsv", "w") as fo:
+            fo.write("mutation_rate\tneff_sampled\tneff_fit\n")
+            for mr, nf in zip(mrs, nfs):
+                fo.write("{0}\t{1}\t{2}\n".format(mr, nf, f(mr, *popt)))
 
-    mdl = dict(zip('abt', popt))
+    except Exception as e:
 
-    print("Fit model a={a}, b={b}, t={t}".format(**mdl))
+        with open("test.tsv", "w") as fo:
+            fo.write("mutation_rate\tneff_sampled")
+            for mr, nf in zip(mrs, nfs):
+                fo.write("{0}\t{1}".format(mr, nf))
+
+        raise e
+
+    mdl = dict(zip('at', popt))
+
+    print("Fit model a={a}, t={t}".format(**mdl))
 
     return mdl
 
@@ -88,22 +98,16 @@ def fit_neff_model(branch_lengths, n_children, n_vertices, n_leaves, ncol, x, se
 def evoldist_for_neff(target_neff, n_leaves, model_parameters=RFIT_PARAMETERS):
     """Compute the correct evolutionary distance for a target Neff
 
-    From our model, we substitute z = D - t and obtain the polynomial:
-    a*z^3 - b*z + log((N - 1) / (Neff - 1) - 1) = 0
+    From our model, we solve for x and obtain:
+    x = t - a*log((N-1) / (Neff - 1) - 1)
 
-    that can be solved using numpy.roots
     """
 
     neffratio = np.log((n_leaves - 1) / (target_neff - 1) - 1)
-    roots = np.roots([model_parameters['a'], 0, -model_parameters['b'], neffratio])
-
-    dists = [np.real(r) + model_parameters['t'] for r in roots if np.abs(np.imag(r)) < 1e-5]
-
-    # only keep real solution and resubstitute D = z + t
-    mutation_rate = min(dists)
+    mutation_rate = model_parameters['t'] - model_parameters['a'] * neffratio
 
     if mutation_rate < 0:
-        raise Exception("Got negative mutation rate {0} for target Neff {1}! (a={a}, b={b}, t={t})".format(mutation_rate, target_neff, **model_parameters))
+        raise Exception("Got negative mutation rate {0} for target Neff {1}! (a={a}, t={t})".format(mutation_rate, target_neff, **model_parameters))
 
     return mutation_rate
 
