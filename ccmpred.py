added weighting, corrected ps, added modelprobs

ccmpred.py
@@ -2,7 +2,9 @@
 import argparse
 import numpy as np
 import sys
+import json
 
+import ccmpred.metadata
 import ccmpred.weighting
 import ccmpred.scoring
 import ccmpred.pseudocounts
@@ -11,6 +13,7 @@ import ccmpred.logo
 import ccmpred.io.alignment as aln
 import ccmpred.centering
 import ccmpred.regularization
+import ccmpred.model_probabilities
 
 import ccmpred.objfun.pll as pll
 import ccmpred.objfun.cd as cd
@@ -27,7 +30,7 @@ CCMpred is a fast python implementation of the maximum pseudo-likelihood class o
 
 ALGORITHMS = {
     "gradient_descent": lambda of, x0, opt: ccmpred.algorithm.gradient_descent.minimize(of, x0, opt.numiter, alpha0=5e-3, alpha_decay=1e1),
-    "conjugate_gradients": lambda of, x0, opt: ccmpred.algorithm.conjugate_gradients.minimize(of, x0, opt.numiter, epsilon=1e-7),
+    "conjugate_gradients": lambda of, x0, opt: ccmpred.algorithm.conjugate_gradients.minimize(of, x0, opt.numiter, epsilon=1e-5),
     "numerical_differentiation": lambda of, x0, opt: ccmpred.algorithm.numdiff.numdiff(of, x0),
 }
 
@@ -69,6 +72,8 @@ def parse_args():
     parser.add_argument("-i", "--init-from-raw", dest="initrawfile", default=None, help="Init potentials from raw file")
     parser.add_argument("-r", "--write-raw", dest="outrawfile", default=None, help="Write potentials to raw file")
     parser.add_argument("-b", "--write-msgpack", dest="outmsgpackfile", default=None, help="Write potentials to MessagePack file")
+    parser.add_argument("-m", "--write-modelprob-msgpack", dest="outmodelprobmsgpackfile", default=None, help="Write model probabilities as MessagePack file")
+    parser.add_argument("-A", "--disable_apc",  dest="disable_apc", action="store_true", default=False, help="Disable average product correction (APC)")
     parser.add_argument("--aln-format", dest="aln_format", default="psicov", help="File format for MSAs [default: \"%(default)s\"]")
     parser.add_argument("--no-logo", dest="logo", default=True, action="store_false", help="Disable showing the CCMpred logo")
 
@@ -86,17 +91,20 @@ def parse_args():
     grp_al.add_argument("--alg-nd", dest="algorithm", action="store_const", const=ALGORITHMS['numerical_differentiation'], help='Debug gradients with numerical differentiation')
 
     grp_wt = parser.add_argument_group("Weighting")
-    grp_wt.add_argument("--wt-simple", dest="weight", action="store_const", const=ccmpred.weighting.weights_simple, default=ccmpred.weighting.weights_simple, help='Use simple weighting (default)')
-    grp_wt.add_argument("--wt-uniform", dest="weight", action="store_const", const=ccmpred.weighting.weights_uniform, help='Use uniform weighting')
+    grp_wt.add_argument("--wt-simple",          dest="weight", action="store_const", const=ccmpred.weighting.weights_simple, default=ccmpred.weighting.weights_simple, help='Use simple weighting (default)')
+    grp_wt.add_argument("--wt-henikoff",        dest="weight", action="store_const", const=ccmpred.weighting.weights_henikoff, help='Use simple Henikoff weighting')
+    grp_wt.add_argument("--wt-henikoff_pair",   dest="weight", action="store_const", const=ccmpred.weighting.weights_henikoff_pair, help='Use Henikoff pair weighting ')
+    grp_wt.add_argument("--wt-uniform",         dest="weight", action="store_const", const=ccmpred.weighting.weights_uniform, help='Use uniform weighting')
 
     grp_rg = parser.add_argument_group("Regularization")
     grp_rg.add_argument("--reg-l2", dest="regularization", action=RegL2Action, type=float, nargs=2, metavar=("LAMBDA_SINGLE", "LAMBDA_PAIR"), default=lambda msa, centering: ccmpred.regularization.L2(10, 0.2 * (msa.shape[1] - 1), centering), help='Use L2 regularization with coefficients LAMBDA_SINGLE, LAMBDA_PAIR * L (default: 10 0.2)')
 
     grp_pc = parser.add_argument_group("Pseudocounts")
-    grp_pc.add_argument("--pc-submat", dest="pseudocounts", action=StoreConstParametersAction, default=ccmpred.pseudocounts.substitution_matrix_pseudocounts, const=ccmpred.pseudocounts.substitution_matrix_pseudocounts, nargs="?", metavar="N", type=float, arg_default=1, help="Use N substitution matrix pseudocounts (default) (by default, N=1)")
-    grp_pc.add_argument("--pc-constant", dest="pseudocounts", action=StoreConstParametersAction, const=ccmpred.pseudocounts.constant_pseudocounts, metavar="N", nargs="?", type=float, arg_default=1, help="Use N constant pseudocounts (by default, N=1)")
-    grp_pc.add_argument("--pc-none", dest="pseudocounts", action="store_const", const=ccmpred.pseudocounts.no_pseudocounts, help="Use no pseudocounts")
-    grp_pc.add_argument("--pc-pair-count", dest="pseudocount_pair_count", default=None, type=int, help="Specify a separate number of pseudocounts for pairwise frequencies (default: use same as single counts)")
+    grp_pc.add_argument("--pc-submat",      dest="pseudocounts", action=StoreConstParametersAction, default=ccmpred.pseudocounts.substitution_matrix_pseudocounts, const=ccmpred.pseudocounts.substitution_matrix_pseudocounts, nargs="?", metavar="N", type=float, arg_default=1, help="Use N substitution matrix pseudocounts (default) (by default, N=1)")
+    grp_pc.add_argument("--pc-constant",    dest="pseudocounts", action=StoreConstParametersAction, const=ccmpred.pseudocounts.constant_pseudocounts, metavar="N", nargs="?", type=float, arg_default=1, help="Use N constant pseudocounts (by default, N=1)")
+    grp_pc.add_argument("--pc-uniform",     dest="pseudocounts", action=StoreConstParametersAction, const=ccmpred.pseudocounts.uniform_pseudocounts, metavar="N", nargs="?", type=float, arg_default=1, help="Use N uniform pseudocounts, e.g 1/21 (by default, N=1)")
+    grp_pc.add_argument("--pc-none",        dest="pseudocounts", action="store_const", const=ccmpred.pseudocounts.no_pseudocounts, help="Use no pseudocounts")
+    grp_pc.add_argument("--pc-pair-count",  dest="pseudocount_pair_count", default=None, type=int, help="Specify a separate number of pseudocounts for pairwise frequencies (default: use same as single counts)")
 
     grp_db = parser.add_argument_group("Debug Options")
     grp_db.add_argument("--write-trajectory", dest="trajectoryfile", default=None, help="Write trajectory to files with format expression")
@@ -156,7 +164,8 @@ def main():
 
     print("\n{0} with code {code} -- {message}".format(condition, **algret))
 
-    res = f.finalize(x)
+    meta = ccmpred.metadata.create(opt, regularization, msa, weights, f, fx, algret)
+    res = f.finalize(x, meta)
 
     if opt.cd_alnfile and hasattr(f, 'msa_sampled'):
         print("Writing sampled alignment to {0}".format(opt.cd_alnfile))
@@ -173,11 +182,18 @@ def main():
         print("Writing msgpack-formatted potentials to {0}".format(opt.outmsgpackfile))
         ccmpred.raw.write_msgpack(opt.outmsgpackfile, res)
 
-    print("Writing summed score matrix to {0}".format(opt.matfile))
-    mat = ccmpred.scoring.frobenius_score(res.x_pair)
-    np.savetxt(opt.matfile, mat)
+    if opt.outmodelprobmsgpackfile:
+        print("Writing msgpack-formatted model probabilties to {0}".format(opt.outmodelprobmsgpackfile))
+        ccmpred.model_probabilities.write_msgpack(opt.outmodelprobmsgpackfile, res, msa, weights, freqs[1], regularization.lambda_pair)
 
-    print()
+    print("Writing summed score matrix (with APC={0}) to {1}".format(not opt.disable_apc, opt.matfile))
+    mat = ccmpred.scoring.frobenius_score(res.x_pair)
+    if not opt.disable_apc:
+        mat = ccmpred.scoring.apc(mat)
+    np.savetxt(opt.matfile, mat)
+    with open(opt.matfile,'a') as f:
+        f.write("#>META> ".encode("utf-8") + json.dumps(meta).encode("utf-8") + b"\n")
+
 
     exitcode = 0 if algret['code'] > 0 else -algret['code']
     sys.exit(exitcode)

ccmpred/pseudocounts.py
@@ -15,29 +15,52 @@ def calculate_frequencies(msa, weights, pseudocount_function, pseudocount_n_sing
     pseudocount_ratio_single = pseudocount_n_single / (nrow + pseudocount_n_single)
     pseudocount_ratio_pair = pseudocount_n_pair / (nrow + pseudocount_n_pair)
 
+    print("Calculating AA Frequencies with " +
+          str(np.round(pseudocount_ratio_single, decimals=5)) +
+          " percent pseudocounts (" + pseudocount_function.__name__+")")
+
+    #normalized with gaps
     single_freq = single_counts / nrow
     pair_freq = pair_counts / nrow
 
+    #normalized with gaps
     pcounts = pseudocount_function(single_freq)
 
+    #remove gaps and renormalize
+    single_freq = degap(single_freq, True)
+    pair_freq   = degap(pair_freq, True)
+
     single_freq_pc = (1 - pseudocount_ratio_single) * single_freq + pseudocount_ratio_single * pcounts
-    pair_freq_pc = ((1 - pseudocount_ratio_pair) ** 2) * (
+    pair_freq_pc = ((1 - pseudocount_ratio_pair) * (1 - pseudocount_ratio_pair)) * (
         pair_freq - single_freq[:, np.newaxis, :, np.newaxis] * single_freq[np.newaxis, :, np.newaxis, :]
     ) + (single_freq_pc[:, np.newaxis, :, np.newaxis] * single_freq_pc[np.newaxis, :, np.newaxis, :])
+
 
     return single_freq_pc, pair_freq_pc
 
 
-def degap(single_freq, keep_dims=False):
-    out = single_freq[:, :20] / (1 - single_freq[:, 20])[:, np.newaxis]
+def degap(freq, keep_dims=False):
+    if len(freq.shape) == 2 :
+        out = freq[:, :20] / (1 - freq[:, 20])[:, np.newaxis]
+    else:
+        out = freq[:, :, :20, :20] / freq[:,:,:20, :20].sum(3).sum(2)[:, :,  np.newaxis, np.newaxis]
 
     if keep_dims:
-        out2 = np.zeros((single_freq.shape[0], 21))
-        out2[:, :20] = out
+        if len(freq.shape) == 2 :
+            out2 = np.zeros((freq.shape[0], 21))
+            out2[:, :20] = out
+        else:
+            out2 = np.zeros((freq.shape[0], freq.shape[1], 21, 21))
+            out2[:, :, :20, :20] = out
         out = out2
 
     return out
 
+
+def uniform_pseudocounts(single_freq):
+    uniform_pc = np.zeros(single_freq.shape)
+    uniform_pc.fill(1./20)
+    return uniform_pc
 
 def constant_pseudocounts(single_freq):
     return np.mean(single_freq, axis=0)[np.newaxis, :]

ccmpred/weighting/__init__.py
@@ -1,6 +1,6 @@
 import numpy as np
-from ccmpred.weighting.cext import count_ids, calculate_weights
-
+from ccmpred.weighting.cext import count_ids, calculate_weights_simple
+import ccmpred.counts
 
 def weights_uniform(msa):
     """Uniform weights"""
@@ -13,4 +13,59 @@ def weights_simple(msa, cutoff=0.8):
     if cutoff >= 1:
         return weights_uniform(msa)
 
-    return calculate_weights(msa, cutoff)
+    return calculate_weights_simple(msa, cutoff)
+
+
+def weights_henikoff(msa):
+    """Henikoff weighting according to Henikoff, S and Henikoff, JG. Position-based sequence weights. 1994"""
+
+    single_counts   = ccmpred.counts.single_counts(msa, None)
+    unique_aa       = (single_counts != 0).sum(1)
+    nrow, ncol = msa.shape
+
+
+    # henikoff = np.zeros(nrow)
+    # for n in range(nrow):
+    #     for l in range(ncol):
+    #         henikoff[n] += 1/(single_counts[l, msa[n][l]] * unique_aa[l])
+    henikoff = np.array([sum(1.0/(single_counts[range(ncol), msa[n]] * unique_aa)) for n in range(nrow)])
+
+    #example from henikoff paper
+    # msa=np.array([[0,1,3,0,6],[0,2,4,0,2],[0,1,4,0,2],[0,1,5,0,0]])
+    # single_counts = np.zeros((5, 7))
+    # single_counts[0,0]=4
+    # single_counts[1,1]=3
+    # single_counts[1,2]=1
+    # single_counts[2,3]=1
+    # single_counts[2,4]=2
+    # single_counts[2,5]=1
+    # single_counts[3,0]=4
+    # single_counts[4,0]=1
+    # single_counts[4,2]=2
+    # single_counts[4,6]=1
+    # unique_aa       = (single_counts != 0).sum(1)
+    # henikoff = np.array([sum(1.0/(single_counts[range(ncol), msa[n]] * unique_aa)) for n in range(nrow)])
+    # array([ 1.33333333,  1.33333333,  1.        ,  1.33333333])
+
+    return henikoff
+
+
+def weights_henikoff_pair(msa):
+    """Henikoff pair weighting"""
+
+    pair_counts   = ccmpred.counts.pair_counts(msa, None)
+    unique_aa     = (pair_counts != 0).sum(3).sum(2)
+    nrow, ncol = msa.shape
+
+    henikoff = np.zeros(nrow)
+    for n in range(nrow):
+        for k in range(ncol-1):
+            for l in range(k+1,ncol):
+                henikoff[n] += 1/(pair_counts[k,l, msa[n][k],msa[n][l]]  * unique_aa[k,l])
+
+
+    return henikoff
+
+
+
+

ccmpred/weighting/cext/__init__.py
@@ -17,8 +17,8 @@ libweighting.count_ids.argtypes = [
     ctypes.c_uint64,    # ncol
 ]
 
-libweighting.calculate_weights.restype = None
-libweighting.calculate_weights.argtypes = [
+libweighting.calculate_weights_simple.restype = None
+libweighting.calculate_weights_simple.argtypes = [
     array_2d_char,      # *msa
     array_1d_double,    # *weights
     ctypes.c_double,    # cutoff
@@ -35,10 +35,10 @@ def count_ids(msa):
     return ids + ids.T - np.diag(ids.diagonal())
 
 
-def calculate_weights(msa, cutoff):
+def calculate_weights_simple(msa, cutoff):
     nrow = msa.shape[0]
     weights = np.zeros((nrow,), dtype='double')
-    libweighting.calculate_weights(msa, weights, cutoff, *msa.shape)
+    libweighting.calculate_weights_simple(msa, weights, cutoff, *msa.shape)
 
     return weights
 