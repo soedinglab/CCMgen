b354198600b990227dd46f7cb6205a57bf56ed56
Susann Vorberg <susann.vorberg@gmail.com>
2017-03-09 18:44:14 +0100

    works for 20 or 21 states

--- ccmpred/centering.py
+++ ccmpred/centering.py
@@ -5,6 +5,11 @@ def calculate(freqs):
     single_freqs, _ = freqs
 
     #single_freqs either normalized with or without gaps --> same result due to subtraction of mean
+
+
+    #hack when usign no pseudo counts to be able to take log of zero counts
+    eps = 1e-10
+    single_freqs[single_freqs<eps]=eps
     lsingle_freqs = np.log(single_freqs)
 
     #subtract mean of non_gapped frequencies
