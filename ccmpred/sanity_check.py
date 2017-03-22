abd921e45d239c305d9a0067c1ffb8f60dc9b26d
Susann Vorberg <susann.vorberg@gmail.com>
2017-03-22 17:57:39 +0100

    checks whether vi and wij sum to 0

---
+++ ccmpred/sanity_check.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def check_single_potentials(x_single, verbose=0):
+
+    if any(np.abs(x_single.sum(1)) > 1e-10):
+        if verbose: print("Warning: Some single potentials do not sum to 0:")
+        for i in range(x_single.shape[0]):
+            sum_vi = np.abs(np.sum(x_single[i]))
+            if sum_vi > 1e-10:
+                if verbose: print "i={0} has sum_a(v_ia)={1}".format(i+1, np.sum(x_single[i]))
+        return 0
+
+    return 1
+
+def check_pair_potentials(x_pair, verbose=0):
+
+    for i in range(x_pair.shape[0]-1):
+        for j in range(i+1, x_pair.shape[0]):
+            sum_wij = np.abs(np.sum(x_pair[i, j]))
+            if sum_wij > 1e-10:
+                if verbose: print "i={0} j={1} has sum_ab(w_ijab)={2}".format(i+1, j+1, np.sum(x_pair[i,j]))
+                return 0
+
+    return 1
+
+
