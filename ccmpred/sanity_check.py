66e9bafae63e0deb2b7662034a827056830bbbda
Susann Vorberg <susann.vorberg@gmail.com>
2017-03-27 17:59:24 +0200

    new function to normalize potentials according to gauge: sum v_i and sum w_ij = 0

--- ccmpred/sanity_check.py
+++ ccmpred/sanity_check.py
@@ -9,6 +9,8 @@ def check_single_potentials(x_single, verbose=0):
             sum_vi = np.abs(np.sum(x_single[i]))
             if sum_vi > 1e-10:
                 if verbose: print "i={0} has sum_a(v_ia)={1}".format(i+1, np.sum(x_single[i]))
+
+
         return 0
 
     return 1
@@ -25,3 +27,20 @@ def check_pair_potentials(x_pair, verbose=0):
     return 1
 
 
+def normalize_potentials(x_single, x_pair):
+    """
+
+    Enforce gauge choice that
+
+    :param x_single:
+    :param x_pair:
+    :return:
+    """
+
+    means = np.mean(np.mean(x_pair, axis=2), axis=2)
+    x_pair_centered = x_pair - means[:, :, np.newaxis, np.newaxis]
+
+    means = np.mean(x_single, axis=1)
+    x_single_centered = x_single - means[:, np.newaxis]
+
+    return x_single_centered, x_pair_centered
