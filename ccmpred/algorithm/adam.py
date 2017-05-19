fixed bug with weighting: ignore_gaps was T

ccmpred/algorithm/adam.py
@@ -56,7 +56,9 @@ class Adam():
 
         self.progress = pr.Progress(plotfile=None,
                                     xnorm_diff=[], max_g=[], alpha=[],
-                                    sum_qij_uneq_1=[], neg_qijab=[], sum_wij_uneq_0=[])
+                                    sum_qij_uneq_1=[], neg_qijab=[], sum_wij_uneq_0=[], sum_deviation_wij=[], mean_deviation_wij=[],
+                                    m=[], sqrtv=[], step=[], norm_g_reg_pair=[]
+                                    )
 
 
     def __repr__(self):
@@ -90,13 +92,16 @@ class Adam():
 
     def minimize(self, objfun, x, plotfile):
 
-        subtitle = "L={0} N={1} Neff={2} <br>".format(objfun.ncol, objfun.nrow, np.round(objfun.neff, decimals=3))
+        diversity = np.sqrt(objfun.neff)/objfun.ncol
+        subtitle = "L={0} N={1} Neff={2} Diversity={3}<br>".format(objfun.ncol, objfun.nrow, np.round(objfun.neff, decimals=3), np.round(diversity,decimals=3))
         subtitle += self.__repr__().replace("\n", "<br>")
         subtitle += objfun.__repr__().replace("\n", "<br>")
         self.progress.plot_options(
             plotfile,
-            [ '||x||', '||x_single||', '||x_pair||','||g||', '||g_single||', '||g_pair||',
-              'sum_qij_uneq_1', 'neg_qijab', 'sum_wij_uneq_0',
+            [ '||x_pair||', #'||x||', '||x_single||',
+              '||g_pair||', #'||g||', '||g_single||',
+              'sum_qij_uneq_1', 'neg_qijab', 'sum_wij_uneq_0', 'sum_deviation_wij', 'mean_deviation_wij',
+              'm','sqrtv','step', 'norm_g_reg_pair',
               'max_g', 'alpha'
               ],
             subtitle
@@ -114,26 +119,21 @@ class Adam():
         }
 
         fx = -1
-        alpha=self.alpha0
-        beta1=self.beta1
-        beta2=self.beta2
-        beta3=self.beta3
-
+        alpha=self.alpha0 * diversity
         for i in range(self.maxit):
 
             fx, gplot, greg = objfun.evaluate(x)
-
             g = gplot + greg
 
             #update moment vectors
-            first_moment    = beta1 * first_moment + (1-beta1) * (g)
-            second_moment   = beta2 * second_moment + (1-beta2) * (g*g)
-            x_moment        = beta3 * x_moment + (1-beta3) * (x)
+            first_moment    = self.beta1 * first_moment + (1-self.beta1) * (g)
+            second_moment   = self.beta2 * second_moment + (1-self.beta2) * (g*g)
+            x_moment        = self.beta3 * x_moment + (1-self.beta3) * (x)
 
             #compute bias corrected moments
-            first_moment_corrected  = first_moment / (1 - np.power(beta1, i+1))
-            second_moment_corrected = second_moment / (1 - np.power(beta2, i+1))
-            x_moment_corrected = x_moment / (1 - np.power(beta3, i+1))
+            first_moment_corrected  = first_moment / (1 - np.power(self.beta1, i+1))
+            second_moment_corrected = second_moment / (1 - np.power(self.beta2, i+1))
+            x_moment_corrected = x_moment / (1 - np.power(self.beta3, i+1))
 
             first_moment_corrected_single, first_moment_corrected_pair = objfun.linear_to_structured(first_moment_corrected, objfun.ncol)
             second_moment_corrected_single, second_moment_corrected_pair = objfun.linear_to_structured(second_moment_corrected, objfun.ncol)
@@ -166,6 +166,9 @@ class Adam():
             gnorm_plot_pair = np.sum(g_plot_pair * g_plot_pair)
             gnorm_plot = np.sqrt(gnorm_plot_single + gnorm_plot_pair)
 
+            g_reg_plot_single, g_reg_plot_pair = objfun.linear_to_structured(greg, objfun.ncol)
+            gnorm_reg_plot_pair = np.sum(g_reg_plot_pair * g_reg_plot_pair)
+
             max_g = np.max(np.abs(g))
 
             #compute number of problems with qij
@@ -186,7 +189,7 @@ class Adam():
             if self.decay and self.it_succesfull_stop_condition > -1:
                     if self.decay_type == "power":
                         alpha *= self.alpha_decay
-                        #beta1 *= 0.9999
+                        #self.beta1 *= 0.9999
                         #beta2 *= 0.9999
                         #beta3 *= 0.9999
                     elif self.decay_type == "lin":
@@ -207,7 +210,6 @@ class Adam():
             #start decay at iteration i
             if self.decay and xnorm_diff < self.start_decay and self.it_succesfull_stop_condition < 0:
                 self.it_succesfull_stop_condition = i
-                #objfun.average_sample_counts = True
 
             #print out (and possiblly plot) progress
             self.progress.log_progress(i + 1,
@@ -216,7 +218,13 @@ class Adam():
                                        xnorm_diff=xnorm_diff, max_g=max_g, alpha=alpha,
                                        sum_qij_uneq_1=problems['sum_qij_uneq_1'],
                                        neg_qijab=problems['neg_qijab'],
-                                       sum_wij_uneq_0=problems['sum_wij_uneq_0']
+                                       sum_wij_uneq_0=problems['sum_wij_uneq_0'],
+                                       sum_deviation_wij=problems['sum_deviation_wij'],
+                                       mean_deviation_wij=problems['mean_deviation_wij'],
+                                       sqrtv=np.sqrt(np.sum(second_moment_corrected_pair)),
+                                       m=np.sqrt(np.sum(first_moment_corrected_pair*first_moment_corrected_pair)),
+                                       step=np.sqrt(np.sum(step_pair*step_pair)),
+                                       norm_g_reg_pair=np.sqrt(gnorm_reg_plot_pair)
                                        )
 
 
@@ -225,9 +233,17 @@ class Adam():
 
                 if xnorm_diff < self.epsilon:
 
-                    if self.qij_condition:
-                        if (problems['sum_qij_uneq_1'] == 0) and (problems['neg_qijab'] == 0) and (wij_diff == 0):
+                    # objfun.compute_avg_samples = True
+                    # self.epsilon *= 1e-10
+                    # print("turn on averaging")
 
+                    if self.qij_condition:
+
+                        # if (wij_diff == 0) and problems['sum_wij_uneq_0'] != 0:
+                        #     #objfun.average_sample_counts = True
+                        #     alpha *= 10
+                        #     print("turn on averaging")
+                        if (problems['sum_qij_uneq_1'] == 0) and (problems['neg_qijab'] == 0) and (wij_diff == 0):
                             ret = {
                             "code": 1,
                             "message": "Stopping condition (xnorm diff < {0} and wij_diff == 0 and #qij violations = 0 ) successfull.".format(

ccmpred/algorithm/conjugate_gradients.py
@@ -2,6 +2,8 @@ import numpy as np
 import ccmpred.logo
 import sys
 import ccmpred.monitor.progress as pr
+import ccmpred.model_probabilities
+
 
 class conjugateGradient():
     """Optimize objective function usign conjugate gradients"""
@@ -16,7 +18,8 @@ class conjugateGradient():
         self.convergence_prev = convergence_prev
 
         self.progress = pr.Progress(plotfile=None,
-                                    fx=[], max_g=[], step=[], n_linesearch=[], rel_diff_fx=[])
+                                    fx=[], max_g=[], step=[], n_linesearch=[], rel_diff_fx=[],
+                                    sum_qij_uneq_1=[], neg_qijab=[], sum_wij_uneq_0=[])
 
     def __repr__(self):
         return "conjugate gradient optimization (ftol={0} max_linesearch={1} alpha_mul={2} wolfe={3}) \n" \
@@ -37,7 +40,9 @@ class conjugateGradient():
         subtitle += self.__repr__().replace("\n", "<br>")
         self.progress.plot_options(
             plotfile,
-            ['fx',  '||x||', '||x_single||', '||x_pair||','||g||', '||g_single||', '||g_pair||', 'max_g', 'step', 'n_linesearch', 'rel_diff_fx'],
+            ['fx',  '||x||', '||x_single||', '||x_pair||','||g||', '||g_single||', '||g_pair||',
+             'sum_qij_uneq_1', 'neg_qijab', 'sum_wij_uneq_0',
+             'max_g', 'step', 'n_linesearch', 'rel_diff_fx'],
             subtitle
         )
         self.progress.begin_process()
@@ -62,7 +67,8 @@ class conjugateGradient():
         self.progress.log_progress(0,
                                    np.sqrt(xnorm_single + xnorm_pair), np.sqrt(xnorm_single), np.sqrt(xnorm_pair),
                                    np.sqrt(gnorm_single + gnorm_pair), np.sqrt(gnorm_single), np.sqrt(gnorm_pair),
-                                   fx=fx, max_g=max_g, step=0, n_linesearch=0, rel_diff_fx=np.nan)
+                                   fx=fx, max_g=max_g, step=0, n_linesearch=0, rel_diff_fx=np.nan,
+                                   sum_qij_uneq_1=0, neg_qijab=0, sum_wij_uneq_0=0)
 
 
         gprevnorm = None
@@ -133,11 +139,20 @@ class conjugateGradient():
 
             iteration += 1
 
+
+            #compute number of problems with qij
+            problems = ccmpred.model_probabilities.get_nr_problematic_qij(
+                objfun.freqs_pair, x_pair, objfun.regularization.lambda_pair, objfun.Nij, epsilon=1e-2, verbose=False)
+
+
             # print out progress
             self.progress.log_progress(iteration,
                                        np.sqrt(xnorm_single + xnorm_pair), np.sqrt(xnorm_single), np.sqrt(xnorm_pair),
                                        np.sqrt(gnorm), np.sqrt(gnorm_single), np.sqrt(gnorm_pair),
-                                       fx=fx, max_g=max_g, step=alpha, n_linesearch=n_linesearch, rel_diff_fx=rel_diff_fx)
+                                       fx=fx, max_g=max_g, step=alpha, n_linesearch=n_linesearch, rel_diff_fx=rel_diff_fx,
+                                       sum_qij_uneq_1=problems['sum_qij_uneq_1'],
+                                       neg_qijab=problems['neg_qijab'],
+                                       sum_wij_uneq_0=problems['sum_wij_uneq_0'])
 
 
 

ccmpred/objfun/cd/__init__.py
@@ -13,7 +13,10 @@ import ccmpred.weighting
 
 class ContrastiveDivergence():
 
-    def __init__(self, msa, freqs, weights, raw, regularization, gibbs_steps=1, persistent=False, min_nseq_factorL=1, pll=False, average_sample_counts=False):
+    def __init__(self, msa, freqs, weights, raw, regularization, gibbs_steps=1, persistent=False, min_nseq_factorL=1,
+                 minibatches=0,
+                 pll=False,
+                 compute_avg_samples=False, num_averages=5, average_freqency=10):
 
 
         if msa.shape[1] != raw.ncol:
@@ -41,22 +44,19 @@ class ContrastiveDivergence():
         self.pll = pll
 
         #whether to compute average counts from last X sampled MSA's
-        self.average_sample_counts=average_sample_counts
-        self.collection_sample_counts_single = deque([])
-        self.collection_sample_counts_pair = deque([])
+        self.compute_avg_samples=compute_avg_samples
+        self.num_averages = num_averages
+        self.average_freqency = average_freqency
+        self.deque_sample_counts_single = deque([], maxlen=num_averages*average_freqency)
+        self.deque_sample_counts_pair = deque([], maxlen=num_averages*average_freqency)
+
+        #whether to use minibatches in each iteration
+        self.minbatches=np.min([minibatches, self.nrow])
 
         # get constant alignment counts
         self.freqs_single, self.freqs_pair = freqs
         self.msa_counts_single = self.freqs_single * self.neff
         self.msa_counts_pair = self.freqs_pair * self.neff
-
-        #intialise sample counts with 0
-        self.sample_counts_single   = np.zeros_like(self.msa_counts_single)
-        self.sample_counts_pair     = np.zeros_like(self.msa_counts_pair)
-
-
-        #do not use pseudo counts!
-        #self.msa_counts_single, self.msa_counts_pair = ccmpred.counts.both_counts(msa, self.weights)
 
         # reset gap counts
         self.msa_counts_single[:, 20] = 0
@@ -73,43 +73,55 @@ class ContrastiveDivergence():
         self.n_samples_msa = 1
 
         # init sample alignment as input MSA
-        self.msa_sampled = self.init_sample_alignment(self.min_nseq_factorL)
-        self.msa_sampled_weights = ccmpred.weighting.weights_simple(self.msa_sampled, ignore_gaps=True)
+        self.msa_sampled = self.init_sample_alignment(self.min_nseq_factorL, self.minbatches)
+        self.msa_sampled_weights = ccmpred.weighting.weights_simple(self.msa_sampled)
 
-    def init_sample_alignment(self, min_nseq_factorL):
+    def compute_statistics(self, alignment):
 
+        weights = ccmpred.weighting.weights_simple(self.msa_sampled)
+        msa_counts_single, msa_counts_pair = ccmpred.counts.both_counts(
+            alignment, weights)
+
+
+        # reset gap counts
+        msa_counts_single[:, 20] = 0
+        msa_counts_pair[:, :, :, 20] = 0
+        msa_counts_pair[:, :, 20, :] = 0
+
+        #non_gapped counts
+        Ni = msa_counts_single.sum(1) + 1e-10
+        Nij = msa_counts_pair.sum(3).sum(2) + 1e-10
+
+        return msa_counts_single, msa_counts_pair, Ni, Nij, weights
+
+
+    def init_sample_alignment(self, min_nseq_factorL, minibatches):
 
         # nr of sequences = min_nseq_factorL * L
         self.min_nseq_factorL = np.max([min_nseq_factorL, 1])
-        n_sequence_min_nseq_factorL =  self.min_nseq_factorL * self.ncol
+        n_sequence_min_nseq_factorL = self.min_nseq_factorL * self.ncol
 
-        #Use multiples of input MSA: at least 1xMSA
-        self.n_samples_msa = int(np.ceil( n_sequence_min_nseq_factorL / float(self.nrow)))
 
-        if self.average_sample_counts:
-            return self.msa.copy()
-        else:
-            seq_id = range(self.nrow) * self.n_samples_msa
-            #seq_id = np.random.choice(self.nrow, n_sequence_min_nseq_factorL)
-            msa_sampled = self.msa[seq_id]
 
-            return msa_sampled.copy()
 
-    def reset_sample_counts(self):
-        #intialise sample counts with 0
-        self.sample_counts_single   = np.zeros_like(self.msa_counts_single)
-        self.sample_counts_pair     = np.zeros_like(self.msa_counts_pair)
+        # Use multiples of input MSA: at least 1xMSA
+        self.n_samples_msa = int(np.ceil(n_sequence_min_nseq_factorL / float(self.nrow)))
+        seq_id = range(self.nrow) * self.n_samples_msa
+        #seq_id = np.random.choice(self.nrow, n_sequence_min_nseq_factorL)
+        msa_sampled = self.msa[seq_id]
+
+        return msa_sampled.copy()
 
     def finalize(self, x, meta):
         x_single, x_pair = self.linear_to_structured(x, self.ncol, add_gap_state=True)
 
         return ccmpred.raw.CCMRaw(self.ncol, x_single, x_pair, meta)
 
-    def gibbs_sample_sequences(self, x):
-        return ccmpred.objfun.cd.cext.gibbs_sample_sequences(self.msa_sampled,  x, 1)
+    def gibbs_sample_sequences(self, x, gibbs_steps):
+        return ccmpred.objfun.cd.cext.gibbs_sample_sequences(self.msa_sampled,  x, gibbs_steps)
 
-    def gibbs_sample_sequences_nogaps(self, x):
-        return ccmpred.objfun.cd.cext.gibbs_sample_sequences_nogaps(self.msa_sampled,  x, 1)
+    def gibbs_sample_sequences_nogaps(self, x, gibbs_steps):
+        return ccmpred.objfun.cd.cext.gibbs_sample_sequences_nogaps(self.msa_sampled,  x, gibbs_steps)
 
     def sample_position_in_sequences(self, x):
         return ccmpred.objfun.cd.cext.sample_position_in_sequences(self.msa_sampled, x)
@@ -197,49 +209,46 @@ class ContrastiveDivergence():
 
         #reset the msa for sampling in caes of CD
         if not self.persistent:
-            self.msa_sampled = self.init_sample_alignment(self.min_nseq_factorL)
-            #self.msa_sampled_weights = ccmpred.weighting.weights_simple(self.msa_sampled)
-
-        #intialise sample counts with 0
-        self.sample_counts_single   = np.zeros_like(self.msa_counts_single)
-        self.sample_counts_pair     = np.zeros_like(self.msa_counts_pair)
-
-        for k in range(self.gibbs_steps):
-
-            if self.pll:
-                self.msa_sampled = self.sample_position_in_sequences(x)
-            else:
-                #Gibbs Sampling of sequences (each position of each sequence will be sampled this often: self.gibbs_steps)
-                self.msa_sampled = self.gibbs_sample_sequences(x)
-                #self.msa_sampled = self.gibbs_sampling_slow(self.msa_sampled, x, self.gibbs_steps)
-
-            #careful with the weights: sum(sample_counts) should equal sum(msa_counts) !
-            sample_counts_single, sample_counts_pair = ccmpred.counts.both_counts(self.msa_sampled, self.msa_sampled_weights)
-
-            #reset gap counts for sampled msa
-            sample_counts_single[:, 20] = 0
-            sample_counts_pair[:, :, :, 20] = 0
-            sample_counts_pair[:, :, 20, :] = 0
-
-            self.sample_counts_single   += sample_counts_single
-            self.sample_counts_pair     += sample_counts_pair
+            self.msa_sampled = self.init_sample_alignment(self.min_nseq_factorL, self.minbatches)
+            if self.minbatches < self.nrow:
+                self.msa_counts_single, self.msa_counts_pair, self.Ni, self.Nij, self.msa_sampled_weights = self.compute_statistics(self.msa_sampled)
 
 
-        # if self.average_sample_counts:
-        #     #sample_counts_single, sample_counts_pair = self.compute_sample_count_averages(sample_counts_single, sample_counts_pair)
-        #     self.sample_counts_single   = 0.99 * self.sample_counts_single + 0.01 * sample_counts_single
-        #     self.sample_counts_pair     = 0.99 * self.sample_counts_pair   + 0.01 * sample_counts_pair
-        # else:
-        #     self.sample_counts_single   = sample_counts_single #0.99 * self.sample_counts_single + 0.01 * sample_counts_single
-        #     self.sample_counts_pair     = sample_counts_pair#0.99 * self.sample_counts_pair + 0.01 * sample_counts_pair
+        if self.pll:
+            self.msa_sampled = self.sample_position_in_sequences(x)
+        else:
+            #Gibbs Sampling of sequences (each position of each sequence will be sampled this often: self.gibbs_steps)
+            self.msa_sampled = self.gibbs_sample_sequences(x, self.gibbs_steps)
+            #self.msa_sampled = self.gibbs_sampling_slow(self.msa_sampled, x, self.gibbs_steps)
 
-        # number of non_gapped counts per position(pair)
-        Ni_sampled  = self.sample_counts_single.sum(1) + 1e-10
-        Nij_sampled = self.sample_counts_pair.sum(3).sum(2) + 1e-10
+
+        #counts from sample
+        sample_counts_single, sample_counts_pair = ccmpred.counts.both_counts(self.msa_sampled, self.msa_sampled_weights)
+
+
+        # reset gap counts
+        sample_counts_single[:, 20] = 0
+        sample_counts_pair[:, :, :, 20] = 0
+        sample_counts_pair[:, :, 20, :] = 0
+
+
+        if self.num_averages > 0:
+            self.deque_sample_counts_single.append(sample_counts_single)
+            self.deque_sample_counts_pair.append(sample_counts_pair)
+
+
+        if (len(self.deque_sample_counts_single) == self.deque_sample_counts_single.maxlen) and self.compute_avg_samples:
+            indices=range(0, self.deque_sample_counts_single.maxlen, self.average_freqency)
+            sample_counts_single = np.array(self.deque_sample_counts_single)[indices].sum(0)
+            sample_counts_pair = np.array(self.deque_sample_counts_pair)[indices].sum(0)
+
+
+        Ni_sampled = sample_counts_single.sum(1) + 1e-10
+        Nij_sampled = sample_counts_pair.sum(3).sum(2) + 1e-10
 
         # normalize counts according to input msa counts
-        sampled_freq_single     = self.sample_counts_single / Ni_sampled[:, np.newaxis]
-        sampled_freq_pair       = self.sample_counts_pair / Nij_sampled[:, :, np.newaxis, np.newaxis]
+        sampled_freq_single     = sample_counts_single / Ni_sampled[:, np.newaxis]
+        sampled_freq_pair       = sample_counts_pair / Nij_sampled[:, :, np.newaxis, np.newaxis]
         sample_counts_single    = sampled_freq_single * self.Ni[:, np.newaxis]
         sample_counts_pair      = sampled_freq_pair * self.Nij[:, :, np.newaxis, np.newaxis]
 
@@ -247,11 +256,9 @@ class ContrastiveDivergence():
         g_single = sample_counts_single - self.msa_counts_single
         g_pair = sample_counts_pair - self.msa_counts_pair
 
-
-
         #sanity check
-        if(np.abs(np.sum(sample_counts_single[0,:20]) - np.sum(self.msa_counts_single[0,:20])) > 1e-5):
-            print("Warning: sample aa counts ({0}) do not equal input msa aa counts ({1})!".format(np.sum(sample_counts_single[0,:20]), np.sum(self.msa_counts_single[0,:20])))
+        if(np.abs(np.sum(sample_counts_single[10,:20]) - np.sum(self.msa_counts_single[10,:20])) > 1e-5):
+            print("Warning: sample aa counts ({0}) do not equal input msa aa counts ({1})!".format(np.sum(sample_counts_single[10,:20]), np.sum(self.msa_counts_single[10,:20])))
 
 
         # set gradients for gap states to 0
@@ -282,9 +289,12 @@ class ContrastiveDivergence():
             "PLL " if (self.pll) else ""
         )
 
-        str += "#samples={0} ({1} x N and {2} x L)  Gibbs steps={3}".format(
+        str += "#samples={0} ({1} x N and {2} x L)  Gibbs steps={3} ".format(
             (self.n_samples_msa * self.nrow), np.round(self.n_samples_msa, decimals=3),  np.round((self.n_samples_msa * self.nrow) / float(self.ncol), decimals=3), self.gibbs_steps
         )
+
+        if(self.compute_avg_samples):
+            str +="#averages={0} #avg_freq={1}".format(self.num_averages, self.average_freqency)
 
         return str
 