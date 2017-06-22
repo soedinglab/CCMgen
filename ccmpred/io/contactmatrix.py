977d3ff1c83c43f0200a30f096280a86aeed0f58
Susann Vorberg <susann.vorberg@gmail.com>
2017-06-22 17:14:20 +0200

    moved scoring functions here

--- ccmpred/io/contactmatrix.py
+++ ccmpred/io/contactmatrix.py
@@ -1,17 +1,30 @@
-import ccmpred.scoring
 import numpy as np
 import json
 import gzip
 
+def frobenius_score(x):
+    print("\nCompute contact map with frobenius norm.")
+    return np.sqrt(np.sum(x * x, axis=(2, 3)))
 
 
-def write_matrix(matfile, res, meta, disable_apc=False):
+def apc(cmat):
+    print("\nApply Average Product Correction(APC).")
+    mean = np.mean(cmat, axis=0)
+    apc_term = mean[:, np.newaxis] * mean[np.newaxis, :] / np.mean(cmat)
+    return cmat - apc_term
 
-    print("\nWriting summed score matrix (with APC={0}) to {1}".format(not disable_apc, matfile))
 
-    mat = ccmpred.scoring.frobenius_score(res.x_pair)
+def compute_matrix(x_pair, disable_apc=False):
+
+    mat = frobenius_score(x_pair)
     if not disable_apc:
-        mat = ccmpred.scoring.apc(mat)
+        mat = apc(mat)
+    return mat
+
+
+def write_matrix(matfile, mat, meta):
+
+    print("\nWriting contact map to {0}".format(matfile))
 
     if matfile.endswith(".gz"):
         with gzip.open(matfile, 'wb') as f:
