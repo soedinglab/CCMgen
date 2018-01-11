1b1a3fb32f474041e53ca390e7507d070be0027d
Susann Vorberg <susann.vorberg@gmail.com>
2018-01-11 12:58:05 +0100

    removed numpy dependency

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-import numpy.distutils.intelccompiler
+#import numpy.distutils.intelccompiler
 from setuptools import setup, Extension, find_packages
 
-def ext(name, sources=[], include_dirs=[], library_dirs=[], libraries=[], extra_compile_args=['-g -fopenmp -std=c99'], extra_link_args=['-g -fopenmp']):
+def ext(name, sources=[], include_dirs=[], library_dirs=[], libraries=[], extra_compile_args=['-g', '-fopenmp', '-std=c99'], extra_link_args=['-g', '-fopenmp']):
     return Extension(name, include_dirs=include_dirs, library_dirs=library_dirs, libraries=libraries, sources=sources, extra_compile_args=extra_compile_args, extra_link_args=extra_link_args)
 
 print find_packages()
 
 setup(
-    name="CCMpred",
+    name="CCMpredPy",
     version="1.0.0",
-    description="Residue-residue Contacts from correlated mutations predicted quickly and precisely",
+    description="Residue-residue contact prediction from correlated mutations predicted quickly and precisely",
     license="AGPLv3",
     author="Susann Vorberg",
     author_email="Susann.Vorberg@gmail.com",
@@ -27,23 +27,8 @@ setup(
             ]
         ),
         ext(
-            'ccmpred.objfun.treecd.cext.libtreecd',
-            include_dirs=['ccmpred/objfun/cd/cext'],
-            sources=[
-                'ccmpred/objfun/cd/cext/cd.c',
-                'ccmpred/objfun/cd/cext/cdutil.c',
-                'ccmpred/objfun/treecd/cext/treecd.c',
-            ]
-        ),
-        ext(
             'ccmpred.counts.libmsacounts',
             sources=['ccmpred/counts/msacounts.c']
-        ),
-        ext(
-            'ccmpred.gaps.cext.libgaps',
-            sources=['ccmpred/gaps/cext/gaps.c'],
-            extra_compile_args=['-g -std=c99'],
-            extra_link_args=['-g'],
         ),
         ext(
             'ccmpred.weighting.cext.libweighting',
