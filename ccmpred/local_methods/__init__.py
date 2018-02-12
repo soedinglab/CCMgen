1adbcde031fa22f1c21a71533b797b0fdcd0b9cd
Susann Vorberg <susann.vorberg@gmail.com>
2018-02-12 12:22:53 +0100

    fixed import for py 3.6

--- ccmpred/local_methods/__init__.py
+++ ccmpred/local_methods/__init__.py
@@ -1,2 +1,2 @@
-from mi import compute_mi, compute_mi_pseudocounts
-from omes import compute_omes, compute_omes_freq
+from ccmpred.local_methods.mi import compute_mi, compute_mi_pseudocounts
+from ccmpred.local_methods.omes import compute_omes, compute_omes_freq
