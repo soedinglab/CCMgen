14332e7d494af184b32ef2e903a3e795c55e0e26
Susann Vorberg <susann.vorberg@gmail.com>
2018-02-13 14:03:25 +0100

    added flags, plotting and persistent contrastive divergence

--- run_ccmpred.py
+++ run_ccmpred.py
@@ -2,6 +2,7 @@
 import argparse
 import sys
 import os
+import numpy as np
 
 from ccmpred import CCMpred
 import ccmpred.logo
@@ -12,7 +13,7 @@ import ccmpred.algorithm.lbfgs as lbfgs
 import ccmpred.algorithm.conjugate_gradients as cg
 import ccmpred.algorithm.numdiff as nd
 import ccmpred.algorithm.adam as ad
-
+import ccmpred.plotting as plotting
 
 EPILOG = """
 CCMpredPy is a fast python implementation of the maximum pseudo-likelihood class of contact prediction methods. 
@@ -26,7 +27,8 @@ OBJ_FUNC = {
         ccm,
         gibbs_steps=opt.cd_gibbs_steps,
         sample_size=opt.sample_size,
-        sample_ref=opt.sample_ref
+        sample_ref=opt.sample_ref,
+        persistent=opt.cd_persistent
     )
 }
 
@@ -81,8 +83,9 @@ def parse_args():
     parser.add_argument("-t", "--num_threads",          dest="num_threads", type=int, default=1, help="Specify the number of threads")
     parser.add_argument("--aln-format",                 dest="aln_format", default="psicov", help="File format for MSAs [default: \"%(default)s\"]")
     parser.add_argument("--no-logo",                    dest="logo", default=True, action="store_false", help="Disable showing the CCMpred logo [default: %(default)s]")
+    parser.add_argument("--do-not-optimize", dest="optimize", action="store_false", default=True, help="Do not optimize potentials. Only available when providing raw couplings file with -i.")
 
-    grp_contact_score = parser.add_argument_group("Contact Score and Corrections")
+    grp_contact_score = parser.add_argument_group("Contact Score")
     grp_contact_score.add_argument("--frobenius",         dest="frob",     action="store_true", default=True,  help="Map 20x20 dimensional coupling matrices to Frobenius norm. [default: %(default)s]")
     grp_contact_score.add_argument("--no_centering_potentials", dest="centering_potentials", action="store_false", default=True, help="Ensure sum(v_i)=0 and sum(wij)=0 by subtracting mean. [default: %(default)s]")
 
@@ -94,17 +97,18 @@ def parse_args():
 
 
     grp_out = parser.add_argument_group("Output Options")
-    grp_out.add_argument("-p", "--plot_opt_progress",       dest="plot_opt_progress", default=False, action="store_true", help="Plot optimization progress as interactive html. [default: %(default)s]")
-    grp_out.add_argument("-b", "--write-binary-raw",        dest="out_binary_raw_file",  default=None, help="Write potentials as binary MessagePack file. [default: %(default)s]")
+    grp_out.add_argument("--write-sample-alignment", dest="sample_alnfile",      default=None, metavar="ALNFILE", help="Write PSICOV-formatted sampled alignment to ALNFILE")
+    grp_out.add_argument("-b", "--write-binary-raw", dest="out_binary_raw_file",  default=None, help="Write potentials as binary MessagePack file. [default: %(default)s]")
 
 
     grp_of = parser.add_argument_group("Objective Functions")
-    grp_of.add_argument("--ofn-pll",             dest="objfun", action="store_const", const="pll", default="pll", help="Use pseudo-log-likelihood(pLL) [default: %(default)s]")
-    grp_of.add_argument("--ofn-cd",              dest="objfun", action="store_const", const="cd", help="Use contrastive divergence (CD)")
-    grp_of.add_argument("--cd-sample_size",      dest="sample_size",    default=10.0,   type=float, help="Setting for CD: multiplier X for sampling at max X * SAMPLE_REF sequences per iteration. [default: %(default)s] ")
-    grp_of.add_argument("--cd-sample_ref",       dest="sample_ref",     default="L",    type=str, choices=['L', 'Neff'], help="Setting for CD: Sample at max SAMPLE_SIZE * [L|Neff] sequences per iteration. Reduces runtime. [default: %(default)s] ")
-    grp_of.add_argument("--cd-gibbs_steps",      dest="cd_gibbs_steps", default=1,      type=int,  help="Setting for CD: Perform this many steps of Gibbs sampling per sequence. [default: %(default)s]")
-    grp_of.add_argument("--cd-fix-v",            dest="fix_v",  action="store_true",  default=False,  help="Set single potentials v=v* and do not optimize single potentials v. [default: %(default)s]")
+    grp_of.add_argument("--ofn-pll",             dest="objfun",         action="store_const",   const="pll", default="pll", help="Use pseudo-log-likelihood(pLL) [default: %(default)s]")
+    grp_of.add_argument("--ofn-cd",              dest="objfun",         action="store_const",   const="cd", help="Use contrastive divergence (CD)")
+    grp_of.add_argument("--cd-sample_size",      dest="sample_size",    type=float,             default=10.0,   help="Setting for CD: multiplier X for sampling at max X * SAMPLE_REF sequences per iteration. [default: %(default)s] ")
+    grp_of.add_argument("--cd-sample_ref",       dest="sample_ref",     type=str,               default="L",    choices=['L', 'Neff'], help="Setting for CD: Sample at max SAMPLE_SIZE * [L|Neff] sequences per iteration. Reduces runtime. [default: %(default)s] ")
+    grp_of.add_argument("--cd-gibbs_steps",      dest="cd_gibbs_steps", type=int,               default=1,      help="Setting for CD: Perform this many steps of Gibbs sampling per sequence. [default: %(default)s]")
+    grp_of.add_argument("--cd-fix-v",            dest="fix_v",          action="store_true",    default=False,  help="Set single potentials v=v* and do not optimize single potentials v. [default: %(default)s]")
+    grp_of.add_argument("--cd-persistent",       dest="cd_persistent",  action="store_true",    default=False,  help="Switch on PERSISTENT CD once the learning rate is small enough (< alpha_0 / 10) [default: %(default)s]")
 
     grp_al = parser.add_argument_group("Optimization Algorithms")
     grp_al.add_argument("--alg-cg", dest="algorithm", action="store_const", const='conjugate_gradients', default='conjugate_gradients', help='Use conjugate gradients (CG) (standard for pLL (default) [default: %(default)s] ')
@@ -153,8 +157,8 @@ def parse_args():
 
 
     grp_gp = parser.add_argument_group("Gap Treatment")
-    grp_gp.add_argument("--max_gap_ratio",  dest="max_gap_ratio",   default=100, type=int, help="Ignore alignment positions with > MAX_GAP_RATIO percent gaps. [default: %(default)s == no removal of gaps]")
-    grp_gp.add_argument("--min_coverage",   dest="min_coverage",    default=0, type=int,   help="Remove sequences that do not cover at least MIN_COVERAGE percent of positions. [default: %(default)s == no removal of sequences]")
+    grp_gp.add_argument("--max-gap-pos",  dest="max_gap_pos",  default=100, type=int, help="Ignore alignment positions with >X% gaps. [default: %(default)s == no removal of gaps]")
+    grp_gp.add_argument("--max-gap-seq",  dest="max_gap_seq",  default=100, type=int, help="Remove sequences with >X% gaps. [default: %(default)s == no removal of sequences]")
 
     grp_pc = parser.add_argument_group("Pseudocounts")
     grp_pc.add_argument("--pc-uniform",     dest="pseudocounts", action="store_const", const="uniform_pseudocounts", default="uniform_pseudocounts",     help="Use uniform pseudocounts, e.g 1/21 [default: %(default)s]")
@@ -165,10 +169,10 @@ def parse_args():
     grp_pc.add_argument("--pc-pair-count",  dest="pseudocount_pair",    default=1, type=int, help="Specify number of pseudocounts for pairwise frequencies [default: %(default)s]")
 
 
-    grp_db = parser.add_argument_group("Debug Options")
-    grp_db.add_argument("--write-cd-alignment", dest="cd_alnfile",      default=None, metavar="ALNFILE", help="Write PSICOV-formatted sampled alignment to ALNFILE")
-    grp_db.add_argument("--do-not-optimize",    dest="optimize",        action="store_false", default=True, help="Do not optimize potentials. Only available when providing raw couplings file with -i.")
-
+    grp_pl = parser.add_argument_group("Plotting Options")
+    grp_pl.add_argument("--plot-aln-stats",             dest="plot_alnstats",        default=False, action="store_true",  help="Plot observed vs model alignment statistics as interactive html")
+    grp_pl.add_argument("--plot-aln-aa-distribution",   dest="plot_aln_aadistribution", default=False, action="store_true",  help="Plot amino acid distribution in alignment asinteractive html")
+    grp_pl.add_argument("--plot-opt-progress",          dest="plot_opt_progress",            default=False, action="store_true",  help="Plot optimization progress as interactive html. [default: %(default)s]")
 
     scores = parser.add_argument_group("Alternative Coevolution Scores")
     scores.add_argument("--compute-omes",       dest="omes",                action="store_true", default=False, help="Compute OMES scores as in Kass and Horovitz 2002. [default: %(default)s]")
@@ -177,12 +181,7 @@ def parse_args():
     scores.add_argument("--mi-normalized",      dest="mi_normalized",       action="store_true", default=False, help="MI option: Compute normalized MI according to Martin et al 2005 . [default: %(default)s]")
     scores.add_argument("--mi-pseudocounts",    dest="mi_pseudocounts",     action="store_true", default=False, help="MI option: Compute MI with pseudocounts . [default: %(default)s]")
 
-
-
     args = parser.parse_args()
-
-    if args.cd_alnfile and args.objfun != "cd":
-        parser.error("--write-cd-alignment is only supported for contrastive divergence!")
 
     if not args.optimize and not args.initrawfile:
         parser.error("--do-not-optimize is only supported when -i (--init-from-raw) is specified!")
@@ -198,34 +197,52 @@ def parse_args():
     if args.plot_opt_progress:
         args.plotfile=".".join(args.matfile.split(".")[:-1])+".opt_progress.html"
 
+    args.plot_aln_aadistribution_file = None
+    if args.plot_aln_aadistribution:
+        args.plot_aln_aadistribution_file = ".".join(args.matfile.split(".")[:-1]) + ".aln_aa_distribution.html"
+
+    args.plot_alnstats_file = None
+    if args.plot_alnstats:
+        args.plot_alnstats_file = ".".join(args.matfile.split(".")[:-1]) + ".empirical_vs_model_alignment_stats.html"
+
     return args
 
 
 def main():
 
-    #Read command line options
+    # read command line options
     opt = parse_args()
 
+    # print logo
     if opt.logo:
         ccmpred.logo.logo()
 
-    #set OMP environment variable for number of threads
+    # set OMP environment variable for number of threads
     os.environ['OMP_NUM_THREADS'] = str(opt.num_threads)
     print("Using {0} threads for OMP parallelization.".format(os.environ["OMP_NUM_THREADS"]))
 
-
+    # instantiate CCMpred
     ccm = CCMpred(opt.alnfile, opt.matfile)
 
-    ##############################
-    ### Setup
-    ##############################
+    # read alignment and possible remove gapped sequences and positions
+    ccm.read_alignment(opt.aln_format, opt.max_gap_pos, opt.max_gap_seq)
 
-    #read alignment and compute amino acid counts and frequencies
-    ccm.read_alignment(opt.aln_format, opt.max_gap_ratio, opt.min_coverage)
+    # compute sequence weights (in order to reduce sampling bias)
     ccm.compute_sequence_weights(opt.weight, opt.wt_ignore_gaps, opt.wt_cutoff)
+
+    # compute amino acid counts and frequencies adding pseudo counts for non-observed amino acids
     ccm.compute_frequencies(opt.pseudocounts, opt.pseudocount_single,  opt.pseudocount_pair)
 
-    #if alternative scores are specified: compute these and exit
+    # plot amino acid distribution per position of alignment
+    if opt.plot_aln_aadistribution:
+        plotting.plot_alignment(
+            ccm.pseudocounts.counts[0],
+            "Amino Acid Distribution in Alignment for {0} (N={1}, L={2}, diversity={3})".format(
+                ccm.protein, ccm.N, ccm.L, np.round(ccm.diversity, decimals=3)),
+            opt.plot_aln_aadistribution_file
+        )
+
+    # if alternative scores are specified: compute these and exit
     if opt.omes:
         ccm.compute_omes(opt.omes_fodoraldrich)
         ccm.write_matrix()
@@ -236,20 +253,17 @@ def main():
         ccm.write_matrix()
         sys.exit(0)
 
-    #setup L2 regularization
+    # setup L2 regularization
     ccm.specify_regularization(opt.lambda_single, opt.lambda_pair_factor, reg_type=opt.reg_type, scaling=opt.scaling, single_prior=opt.single_prior)
 
-    #intialise single and pair potentials either:
+    # intialise single and pair potentials either:
     #   - according to regularization priors
-    #   - from file
+    #   - from initrawfile (accounting for removal of many gapped positions, if applicable)
     ccm.intialise_potentials(opt.initrawfile)
 
 
-    ##############################
-    ### Optimize objective function (pLL or CD) with optimization algorithm (CG, GD or ADAM)
-    ##############################
+    # optimize objective function (pLL or CD/PCD) with optimization algorithm (LBFGS, CG, GD or ADAM)
     if opt.optimize:
-
         # specify objective function
         objfun = OBJ_FUNC[opt.objfun](opt, ccm)
 
@@ -259,7 +273,9 @@ def main():
         #minimize objective function with optimizer
         ccm.minimize(objfun, alg)
     else:
-        print("\nDo not optimize but load couplings from binary raw file {0}\n".format(opt.initrawfile))
+        print("\nDo not optimize but use model parameters provided by {0}\n".format(opt.initrawfile))
+
+
 
     ##############################
     ### Post Processing
@@ -269,7 +285,7 @@ def main():
     # TODO: other scores can be added ...
     ccm.compute_contact_matrix(recenter_potentials=opt.centering_potentials, frob=opt.frob)
 
-    # and bias correction to contact score
+    # compute corrected contact maps (removing entropy/phylogenetic biases)
     ccm.compute_correction(
         apc=opt.apc,
         entropy_correction=opt.entropy_correction,
@@ -277,15 +293,17 @@ def main():
         sergeys_jec=opt.sergeys_joint_entropy_correction
     )
 
+    # sample an alignment according to model and save to file
+    if opt.sample_alnfile or opt.plot_alnstats:
+        ccm.write_sampled_alignment(opt.sample_alnfile, opt.plot_alnstats_file)
+
     #specify meta data, and write (corrected) contact matrices to files
     ccm.write_matrix()
 
-    if opt.cd_alnfile and hasattr(ccm.f, 'msa_sampled'):
-        ccm.write_sampled_alignment(opt.cd_alnfile)
-
-
+    # write model parameters in binary format
     if opt.out_binary_raw_file:
         ccm.write_binary_raw(opt.out_binary_raw_file)
+
 
     exitcode = 0
     if opt.optimize:
