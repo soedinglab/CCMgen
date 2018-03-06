f99b10d5f4e07547aa30b9616a74819bd2c0612c
Susann Vorberg <susann.vorberg@gmail.com>
2018-03-06 15:36:10 +0100

    either ccmpred or ccmgen logo

--- ccmpred/logo.py
+++ ccmpred/logo.py
@@ -1,22 +1,38 @@
+# coding: utf-8
 import ccmpred
 import sys
 
-
 is_tty = (sys.stdin.isatty()) and (sys.stdout.isatty())
 
+LOGOS = {}
+LOGOS['ccmpred', True] = """
+  \x1b[32m┏━╸┏━╸┏┳┓\x1b[34m┏━┓┏━┓┏━╸╺┳┓\x1b[0m  version {0}
+  \x1b[32m┃  ┃  ┃┃┃\x1b[34m┣━┛┣┳┛┣╸  ┃┃\x1b[0m  Seemayer and Soeding (2016)
+  \x1b[32m┗━╸┗━╸╹ ╹\x1b[34m╹  ╹┗╸┗━╸╺┻┛\x1b[0m  https://github.com/soedinglab
+"""
 
-def logo(color=is_tty):
+LOGOS['ccmpred', False] = """
+  ┏━╸┏━╸┏┳┓┏━┓┏━┓┏━╸╺┳┓  version {0}
+  ┃  ┃  ┃┃┃┣━┛┣┳┛┣╸  ┃┃  Seemayer and Soeding (2016)
+  ┗━╸┗━╸╹ ╹╹  ╹┗╸┗━╸╺┻┛  https://github.com/soedinglab
+"""
+
+
+LOGOS['ccmgen', True] = """
+  \x1b[32m┏━╸┏━╸┏┳┓\x1b[34m┏━╸┏━╸┏┓╻\x1b[0m  version {0}
+  \x1b[32m┃  ┃  ┃┃┃\x1b[34m┃╺┓┣╸ ┃┗┫\x1b[0m  Seemayer and Soeding (2016)
+  \x1b[32m┗━╸┗━╸╹ ╹\x1b[34m┗━┛┗━╸╹ ╹\x1b[0m  https://github.com/soedinglab
+"""
+
+LOGOS['ccmgen', False] = """
+  ┏━╸┏━╸┏┳┓┏━╸┏━╸┏┓╻  version {0}
+  ┃  ┃  ┃┃┃┃╺┓┣╸ ┃┗┫  Seemayer and Soeding (2016)
+  ┗━╸┗━╸╹ ╹┗━┛┗━╸╹ ╹  https://github.com/soedinglab
+"""
+
+
+
+def logo(what_for="ccmpred", color=is_tty):
     version = ccmpred.__version__
 
-    if color:
-        print(" _____ _____ _____               _")
-        print("|\x1b[30;42m     |     |     |\x1b[0m___ ___ ___ _|\x1b[30;44m |\x1b[0m")
-        print("|\x1b[30;42m   --|   --| | | |\x1b[44m . |  _| -_| . |\x1b[0m")
-        print("|\x1b[30;42m_____|_____|_|_|_|\x1b[44m  _|_|\x1b[0m \x1b[30;44m|___|___|\x1b[0m version {0}".format(version))
-        print("                  |\x1b[30;44m_|\x1b[0m\n")
-    else:
-        print(" _____ _____ _____               _")
-        print("|     |     |     |___ ___ ___ _| |")
-        print("|   --|   --| | | | . |  _| -_| . |")
-        print("|_____|_____|_|_|_|  _|_| |___|___| version {0}".format(version))
-        print("                  |_|\n")
+    print(LOGOS[what_for, color].format(version))
