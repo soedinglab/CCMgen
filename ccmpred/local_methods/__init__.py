23c7dfccbae9122542237585ea7a9d2b8aa0a213
Susann Vorberg <susann.vorberg@gmail.com>
2018-03-06 13:17:31 +0100

    renamed to locmeth

--- ccmpred/local_methods/__init__.py
+++
@@ -1,2 +0,0 @@
-from ccmpred.local_methods.mi import compute_mi, compute_mi_pseudocounts
-from ccmpred.local_methods.omes import compute_omes, compute_omes_freq
---
+++ ccmpred/locmeth/__init__.py
@@ -0,0 +1,2 @@
+from ccmpred.locmeth.mi import compute_mi, compute_mi_pseudocounts
+from ccmpred.locmeth.omes import compute_omes, compute_omes_freq
