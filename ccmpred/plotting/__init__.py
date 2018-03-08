a8be1d61b3709208881a4cb400d0853fe0c1ae7c
Susann Vorberg <susann.vorberg@gmail.com>
2018-03-08 13:33:54 +0100

    moved contact map plotting functions from contact map plotting script

--- ccmpred/plotting/__init__.py
+++ ccmpred/plotting/__init__.py
@@ -3,12 +3,230 @@ from plotly.offline import plot as plotly_plot
 from plotly import tools
 import ccmpred.io as io
 import numpy as np
+import scipy.stats
+import colorlover as cl
+
+
+def plot_percentage_gaps_per_position(single_freq, plot_file=None):
+
+    L = single_freq.shape[0]
+
+    #compute percentage of gaps
+    gaps = single_freq[:, 20] / single_freq.sum(1)
+
+    #normalized entropy
+    entropy_per_position = scipy.stats.entropy(single_freq.transpose(),base=2)
+    entropy_per_position /= np.max(entropy_per_position)
+
+
+    #create plot
+    data = []
+    data.append(
+        go.Scatter(
+            x=[x for x in range(1,L+1)],
+            y=gaps,
+            name = "percentage of gaps",
+            mode="Lines",
+            line=dict(width=3)
+        )
+    )
+
+    data.append(
+        go.Scatter(
+            x=[x for x in range(1,L+1)],
+            y=entropy_per_position,
+            name = "relative Entropy",
+            mode="Lines",
+            line=dict(width=3)
+        )
+    )
+
+    layout = {
+        'title':"Percentage of gaps and Entropy in alignment",
+        'xaxis':{'title':"Alignment Position"},
+        'yaxis':{'title':"Percentage of Gaps/Entropy"},
+        'font':{'size':18}
+    }
+
+    plot = {'data': data, 'layout': layout}
+    if plot_file is None:
+        return plot
+    else:
+        plotly_plot(plot, filename=plot_file, auto_open=False)
+
+def plot_contact_map_someScore_plotly(plot_matrix, title, seqsep, gaps_percentage_plot=None, plot_file=None):
+
+    # sort matrix by confidence score
+    plot_matrix.sort_values(by='confidence', ascending=False, inplace=True)
+    L = np.max(plot_matrix[['residue_i', 'residue_j']].values)
+
+    data = []
+
+    # add predicted contact map
+    data.append(
+        go.Heatmap(
+            x=plot_matrix.residue_i.tolist(),
+            y=plot_matrix.residue_j.tolist(),
+            z=plot_matrix.confidence.tolist(),
+            name='predicted',
+            colorscale='Greys', reversescale=True,
+            colorbar=go.ColorBar(
+                x=1.02,
+                y=0.4,
+                yanchor='bottom',
+                len=0.4,
+                title="Score"
+            )
+        )
+    )
+    data.append(
+        go.Heatmap(
+            x=plot_matrix.residue_j.tolist(),
+            y=plot_matrix.residue_i.tolist(),
+            z=plot_matrix.confidence.tolist(),
+            name='predicted'
+        )
+    )
+
+
+    # if distances and class are available
+    if 'contact' in plot_matrix and 'distance' in plot_matrix:
+
+        # define true and false positives among the L/5 highest scores
+        sub_L5_true = plot_matrix.query('distance > 0').head(int(L / 5)).query('contact > 0')
+        sub_L5_false = plot_matrix.query('distance > 0').head(int(L / 5)).query('contact < 1')
+
+        if len(sub_L5_true) > 0:
+            # Mark TP and FP in the plot with little crosses
+            tp = go.Scatter(
+                x=sub_L5_true['residue_i'].tolist() + sub_L5_true['residue_j'].tolist(),
+                y=sub_L5_true['residue_j'].tolist() + sub_L5_true['residue_i'].tolist(),
+                mode='markers',
+                marker=dict(
+                    symbol=134,
+                    color="green",
+                    line=dict(width=2),
+                    size=12
+                ),  # size_tp, sizeref=np.max([size_tp + size_fp])/15, sizemode = 'diameter'),
+                name="TP (L/5)",
+                hoverinfo="none"
+            )
+
+        # 'rgb(255,247,188)', 'rgb(254,196,79)'
+        green_yello_red = ['rgb(254,196,79)', 'rgb(222,45,38)']
+        max_tp = 8
+        max_fp = np.max(plot_matrix[plot_matrix.contact < 1]['distance'])
+        fp_distance_range = int(np.ceil((max_fp - max_tp) / 10.0) * 10)
+        green_yello_red_interpolated = cl.interp(green_yello_red, fp_distance_range)
+        data_color = [green_yello_red_interpolated[int(x - max_tp)] for x in sub_L5_false['distance']]
+
+        if len(sub_L5_false) > 0:
+            fp = go.Scatter(
+                x=sub_L5_false['residue_i'].tolist() + sub_L5_false['residue_j'].tolist(),
+                y=sub_L5_false['residue_j'].tolist() + sub_L5_false['residue_i'].tolist(),
+                mode='markers',
+                marker=dict(
+                    symbol=134,
+                    # color="red",
+                    color=data_color * 2,
+                    colorscale=green_yello_red_interpolated,
+                    line=dict(width=2),
+                    size=12
+                ),  # size_fp, sizeref=np.max([size_tp + size_fp])/15, sizemode = 'diameter'),
+                name="FP (L/5)",
+                hoverinfo="none"
+            )
+
+        # colorscale from red (small distance) to blue(large distance)
+        zmax = np.max(plot_matrix.distance)
+        percent_at_contact_thr = 8 / zmax
+        distance_colorscale = [[0, 'rgb(128, 0, 0)'], [percent_at_contact_thr, 'rgb(255, 255, 255)'],
+                               [1, 'rgb(22, 96, 167)']]
+
+        # define triangle on opposite site of Predictions
+        heatmap_observed = go.Heatmap(
+            x=plot_matrix.residue_j.tolist(),
+            y=plot_matrix.residue_i.tolist(),
+            z=plot_matrix.distance.tolist(),
+            name='observed',
+            zmin=0,
+            zmax=zmax,
+            colorscale=distance_colorscale,
+            # colorscale='Greys', reversescale=True,
+            # colorscale=distance_colors_interpol, reversescale=True,
+            colorbar=go.ColorBar(
+                x=1.02,
+                y=0,
+                yanchor='bottom',
+                len=0.4,
+                title="Distance [A]")
+        )
+
+        # put all plot elements in data list
+        data[1] = heatmap_observed
+
+        if len(sub_L5_true) > 0:
+            data.append(tp)
+        if len(sub_L5_false) > 0:
+            data.append(fp)
+
+
+    # add diagonal and diagonals marking sequence separation
+    data.append(go.Scatter(x=[0, L], y=[0, L], mode='lines', line=dict(color=('rgb(0, 0, 0)'), width=4), hoverinfo=None,
+                           showlegend=False))
+    data.append(
+        go.Scatter(x=[0, L - seqsep + 1], y=[seqsep - 1, L], mode='lines', line=dict(color=('rgb(0, 0, 0)'), width=2),
+                   showlegend=False))
+    data.append(
+        go.Scatter(x=[seqsep - 1, L], y=[0, L - seqsep + 1], mode='lines', line=dict(color=('rgb(0, 0, 0)'), width=2),
+                   showlegend=False))
+
+
+    fig = tools.make_subplots(rows=2, cols=1, shared_xaxes=True, print_grid=False)
+
+
+    if gaps_percentage_plot is not None:
+        for trace in gaps_percentage_plot['data']:
+            fig.append_trace(trace, 1, 1)
+
+
+    for trace in data:
+        fig.append_trace(trace, 2, 1)
+
+    fig['layout']['title'] = title
+    fig['layout']['legend'] = {'x': 1.02, 'y': 1}  # places legend to the right of plot
+
+    fig['layout']['xaxis1']['title'] = 'j'
+    fig['layout']['xaxis1']['range'] = [0.5, L + 0.5]
+    fig['layout']['xaxis1']['domain'] = [0.0, 1.0]
+    fig['layout']['xaxis1']['zeroline'] = False
+
+    fig['layout']['yaxis2']['title'] = 'i'
+    fig['layout']['yaxis2']['range'] = [0.5, L + 0.5]
+    fig['layout']['yaxis2']['domain'] = [0.0, 1.0]
+    fig['layout']['yaxis2']['scaleanchor'] = "x"
+    fig['layout']['yaxis2']['scaleratio'] = 1.0
+    fig['layout']['yaxis2']['zeroline'] = False
+
+    fig['layout']['font']['size'] = 18
+
+    #percentage gaps and entropy plot
+    if gaps_percentage_plot is not None:
+        fig['layout']['yaxis2']['domain'] = [0.0, 0.9]
+        fig['layout']['xaxis1']['domain'] = [0.0, 0.9]
+        fig['layout']['yaxis1']['domain'] = [0.9, 1.0]
+
+
+
+    if plot_file:
+        plotly_plot(fig, filename=plot_file, auto_open=False)
+    else:
+        return fig
 
 def plot_empirical_vs_model_statistics(
         single_freq_observed, single_freq_sampled,
         pairwise_freq_observed, pairwise_freq_sampled,
         title, plot_out, log=False):
-
 
     L = single_freq_observed.shape[0]
 
@@ -171,7 +389,6 @@ def plot_empirical_vs_model_statistics(
 
 
     plotly_plot(fig, filename=plot_out, auto_open=False, link_text='')
-
 
 def plot_alignment(aa_counts_single, title, plot_file, freq=True):
 
