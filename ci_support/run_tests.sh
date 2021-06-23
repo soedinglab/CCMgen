35f60ba1490dc5515fa551b1721caee1ad197be8
Andonis <51485575+odannis@users.noreply.github.com>
2021-06-23 17:39:35 +0200

    Update run_tests.sh

--- ci_support/run_tests.sh
+++ ci_support/run_tests.sh
@@ -3,10 +3,7 @@ ccmgen --tree-newick ci_support/phylo.newick --aln-format psicov --mutation-rate
 ccmgen --tree-newick ci_support/phylo.newick --aln-format fasta --mutation-rate 1 --num-threads 1 ci_support/mrf_params.braw.gz sequences.msa
 
 ## New test
-coupling = ${ci_support/mrf_params.braw.gz}
-seq0_file= ${ci_support/seq0_file.fasta}
+python random_start_sequence.py ci_support/mrf_params.braw.gz ci_support/seq0_file.fasta
 
-python random_start_sequence.py $coupling $seq0_file
-
-ccmgen --tree-newick ci_support/phylo.newick --seq0-file $seq0_file  --mutation-rate 1 --num-threads 1 $coupling sequences.msa
-ccmgen --tree-newick ci_support/phylo.newick --seq0-file $seq0_file  --mutation-rate-neff --num-threads 1 $coupling sequences.msa
+ccmgen --tree-newick ci_support/phylo.newick --seq0-file ci_support/seq0_file.fasta  --mutation-rate 1 --num-threads 1 ci_support/mrf_params.braw.gz sequences.msa
+ccmgen --tree-newick ci_support/phylo.newick --seq0-file ci_support/seq0_file.fasta  --mutation-rate-neff --num-threads 1 ci_support/mrf_params.braw.gz sequences.msa
