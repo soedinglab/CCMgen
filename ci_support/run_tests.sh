d1c46c3db92fe4cfe35df0c3c059739c166099bd
Andonis <51485575+odannis@users.noreply.github.com>
2021-06-23 20:59:01 +0200

    Update run_tests.sh

--- ci_support/run_tests.sh
+++ ci_support/run_tests.sh
@@ -1,10 +1,10 @@
 set -e
+## New test
+python ci_support/random_start_sequence.py ci_support/1atzA.braw.gz ci_support/seq0_file.fasta
+
+ccmgen --tree-newick ci_support/phylo.newick --seq0-file ci_support/seq0_file.fasta  --mutation-rate 1 --num-threads 1 ci_support/1atzA.braw.gz sequences.msa
+ccmgen --tree-newick ci_support/phylo.newick --seq0-file ci_support/seq0_file.fasta  --mutation-rate-neff --num-threads 1 --alnfile ci_support/1atzA.fas  ci_support/1atzA.braw.gz sequences.msa
+
+##Old test
 ccmgen --tree-newick ci_support/phylo.newick --aln-format psicov --mutation-rate 1 --num-threads 1 ci_support/mrf_params.braw.gz sequences.msa
 ccmgen --tree-newick ci_support/phylo.newick --aln-format fasta --mutation-rate 1 --num-threads 1 ci_support/mrf_params.braw.gz sequences.msa
-
-## New test
-python ci_support/random_start_sequence.py exemple/1atzA.braw.gz ci_support/seq0_file.fasta
-
-ccmgen --tree-newick ci_support/phylo.newick --seq0-file ci_support/seq0_file.fasta  --mutation-rate 1 --num-threads 1 exemple/1atzA.braw.gz sequences.msa
-ccmgen --tree-newick ci_support/phylo.newick --seq0-file ci_support/seq0_file.fasta  --mutation-rate-neff --num-threads 1 --alnfile exemple/1atzA.fas  exemple/1atzA.braw.gz sequences.msa
-
