02a25b24ba9012c5efa418ca44339f610d7de2aa
Andonis <51485575+odannis@users.noreply.github.com>
2021-06-23 17:31:29 +0200

    Update run_tests.sh

--- ci_support/run_tests.sh
+++ ci_support/run_tests.sh
@@ -6,7 +6,7 @@ ccmgen --tree-newick ci_support/phylo.newick --aln-format fasta --mutation-rate 
 coupling = ${ci_support/mrf_params.braw.gz}
 seq0_file= ${ci_support/seq0_file.fasta}
 
-python3 random_start_sequence.py $coupling $seq0_file
+python random_start_sequence.py $coupling $seq0_file
 
 ccmgen --tree-newick ci_support/phylo.newick --seq0-file $seq0_file  --mutation-rate 1 --num-threads 1 $coupling sequences.msa
 ccmgen --tree-newick ci_support/phylo.newick --seq0-file $seq0_file  --mutation-rate-neff --num-threads 1 $coupling sequences.msa
