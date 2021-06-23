35cebc6a336e94971c5a785101af293f938a378f
Andonis <51485575+odannis@users.noreply.github.com>
2021-06-23 18:10:37 +0200

    Create random_start_sequence.py

---
+++ ci_support/random_start_sequence.py
@@ -0,0 +1,29 @@
+import ccmpred.raw
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
+import random
+import sys
+
+def write_new_tree_for_ccmgen(file_coupling : str, file_name : str):
+    
+    raw = ccmpred.raw.parse_msgpack(file_coupling)
+    Field = raw.x_single
+    size_prot = Field.shape[0]
+    
+    sequence = ''.join([random.choice('ACDEFGHIKLMNPQRSTVWY-') for x in range(size_prot)]) 
+
+    print("Random sequence : %s"%sequence)
+    
+    record = SeqRecord(
+        Seq(sequence),
+        id="ID_0.1",
+        name="RandomSequences",
+        description="Random Sequences for the root of a phylogeny tree",
+    )
+
+    SeqIO.write(record, file_name, "fasta")
+
+if __name__ == '__main__':
+    # Map command line arguments to function arguments.
+    write_new_tree_for_ccmgen(*sys.argv[1:])
