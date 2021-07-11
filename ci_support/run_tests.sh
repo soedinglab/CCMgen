e6f07a9e60efa173797935b571c9e727fbb6a8d6
Andonis <51485575+odannis@users.noreply.github.com>
2021-07-11 15:20:15 +0200

    Update run_tests.sh

--- ci_support/run_tests.sh
+++ ci_support/run_tests.sh
@@ -3,7 +3,8 @@ set -e
 python ci_support/random_start_sequence.py ci_support/1atzA.braw.gz ci_support/seq0_file.fasta
 
 ccmgen --tree-newick ci_support/1atzA_rootname.tree --seq0-file ci_support/seq0_file.fasta  --mutation-rate 1 --num-threads 1 ci_support/1atzA.braw.gz sequences.msa
-ccmgen --tree-newick ci_support/1atzA_rootname.tree --seq0-file ci_support/seq0_file.fasta  --mutation-rate-neff --num-threads 1 --alnfile ci_support/1atzA.fas  ci_support/1atzA.braw.gz sequences.msa
+## Commented to reduce time for test
+#ccmgen --tree-newick ci_support/1atzA_rootname.tree --seq0-file ci_support/seq0_file.fasta  --mutation-rate-neff --num-threads 1 --alnfile ci_support/1atzA.fas  ci_support/1atzA.braw.gz sequences.msa
 
 ##Old test
 ccmgen --tree-newick ci_support/phylo.newick --aln-format psicov --mutation-rate 1 --num-threads 1 ci_support/mrf_params.braw.gz sequences.msa
